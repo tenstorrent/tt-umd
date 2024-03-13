bfb095f8010c77a5284b4708884169732946a968
Joel Smith <joelsmith@tenstorrent.com>
2024-03-13 12:27:52 -0700

    Remove contraption for suppressing test output

--- tests/unit_test_main.cpp
+++ tests/unit_test_main.cpp
@@ -4,9 +4,7 @@
 
 #include "gtest/gtest.h"
 
-#include "gtest_initializer.hpp"
-
 int main(int argc, char **argv) {
-  initialize_gtest(argc, argv);
+  testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }

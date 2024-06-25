2375b5e9c65e9013e3dc0815b6143ccdfc5871bb
Pavle Janevski <pjanevski@tenstorrent.com>
2024-06-25 15:47:40 +0000

    Move dynamic mem tlbs to WC region

--- device/tt_silicon_driver.cpp
+++ device/tt_silicon_driver.cpp
@@ -3063,8 +3063,8 @@ void *tt_SiliconDevice::channel_0_address(std::uint32_t offset, std::uint32_t de
     // Temporary hack for blackhole bringup.
     if (arch_name == tt::ARCH::BLACKHOLE) {
         // Use 195th 2MB TLB onwards.
-        // TLBs up to 195 are used as static or fallback TLBs.
-        const std::uint32_t TLB_CH0_START = 195;
+        // TLBs up to 184 are used as static or fallback TLBs.
+        const std::uint32_t TLB_CH0_START = 184;
         bar0_offset = offset - architecture_implementation->get_dram_channel_0_peer2peer_region_start()
                         + architecture_implementation->get_dynamic_tlb_2m_base() + (TLB_CH0_START * architecture_implementation->get_dynamic_tlb_2m_size());
 

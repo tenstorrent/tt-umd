3d50f3dd8ac91201010168761b0dc8634ded6862
Vincent Tang <vtang@tenstorrent.com>
2024-07-02 22:15:20 +0000

    TT-Metal VCS simulator support - Add tt_SimulationDevice and tt_SimulationHost - Unit and sweep gtests for tt_SimulationDevice - Update CMake support w/ CPM for: NNG, Flatbuffers, libuv - Add flatbuffer schema for tt_SimulationDevice

---
+++ cmake/dependencies.cmake
@@ -0,0 +1,59 @@
+
+set(ENV{CPM_SOURCE_CACHE} "${PROJECT_SOURCE_DIR}/.cpmcache")
+
+include(${PROJECT_SOURCE_DIR}/cmake/CPM.cmake)
+
+############################################################################################################################
+# google test
+############################################################################################################################
+CPMAddPackage(
+    NAME googletest
+    GITHUB_REPOSITORY google/googletest
+    GIT_TAG v1.13.0
+    VERSION 1.13.0
+    OPTIONS "INSTALL_GTEST OFF"
+    QUIET
+)
+
+############################################################################################################################
+# Nanomsg
+############################################################################################################################
+CPMAddPackage(
+    NAME nanomsg
+    GITHUB_REPOSITORY nanomsg/nng
+    GIT_TAG v1.8.0
+    OPTIONS
+        "BUILD_SHARED_LIBS ON"
+        "NNG_TESTS OFF"
+        "NNG_TOOLS OFF"
+    QUIET
+)
+
+############################################################################################################################
+# Flatbuffers
+############################################################################################################################
+CPMAddPackage(
+    NAME flatbuffers
+    GITHUB_REPOSITORY google/flatbuffers
+    GIT_TAG v24.3.25
+    OPTIONS
+        "FLATBUFFERS_BUILD_FLATC OFF"
+        "FLATBUFFERS_BUILD_TESTS OFF"
+        "FLATBUFFERS_INSTALL OFF"
+        "FLATBUFFERS_BUILD_FLATLIB OFF"
+        "FLATBUFFERS_SKIP_MONSTER_EXTRA ON"
+        "FLATBUFFERS_STRICT_MODE ON"
+    QUIET
+)
+
+############################################################################################################################
+# libuv (for process management)
+############################################################################################################################
+CPMAddPackage(
+    NAME libuv
+    GITHUB_REPOSITORY libuv/libuv
+    GIT_TAG v1.48.0
+    OPTIONS
+        "LIBUV_BUILD_TESTS OFF"
+    DOWNLOAD_ONLY TRUE
+)
---
+++ device/simulation/tt_simulation_host.hpp
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: (c) 2023 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+#include <vector>
+#include <cstdint>
+
+#include <nng/nng.h>
+#include <nng/protocol/pair1/pair.h>
+
+#include "device/tt_xy_pair.h"
+
+#define NNG_SOCKET_PREFIX "ipc:///tmp/"
+
+class tt_SimulationHost {
+public:
+    tt_SimulationHost();
+    ~tt_SimulationHost();
+
+    void start_host();
+    void send_to_device(uint8_t *buf, size_t buf_size);
+    size_t recv_from_device(void **data_ptr);
+private:
+    nng_socket host_socket;
+    nng_dialer host_dialer;
+};

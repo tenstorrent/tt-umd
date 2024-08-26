4f5cd94e8cb0ee460ff61b84de77e7e8dc9fc25a
Vincent Tang <vtang@tenstorrent.com>
2024-08-26 20:51:16 +0000

    Delete epoch/erisc and some random vars

--- device/tt_device.h
+++ device/tt_device.h
@@ -805,10 +805,6 @@ class tt_SiliconDevice: public tt_device
     static constexpr std::uint32_t EPOCH_ETH_CORES_MASK = (EPOCH_ETH_CORES_FOR_NON_MMIO_TRANSFERS-1);
 
     int active_core = NON_EPOCH_ETH_CORES_START_ID;
-    int active_core_epoch = EPOCH_ETH_CORES_START_ID;
-    bool erisc_q_ptrs_initialized = false;
-    std::vector<std::uint32_t> erisc_q_ptrs_epoch[NUM_ETH_CORES_FOR_NON_MMIO_TRANSFERS];
-    bool erisc_q_wrptr_updated[NUM_ETH_CORES_FOR_NON_MMIO_TRANSFERS];
     std::vector< std::vector<tt_cxy_pair> > remote_transfer_ethernet_cores;
     bool flush_non_mmio = false;
     bool non_mmio_transfer_cores_customized = false;
@@ -831,8 +827,6 @@ class tt_SiliconDevice: public tt_device
     std::unordered_map<std::string, std::int32_t> dynamic_tlb_config = {};
     std::unordered_map<std::string, uint64_t> dynamic_tlb_ordering_modes = {};
     std::map<std::set<chip_id_t>, std::unordered_map<chip_id_t, std::vector<std::vector<int>>>> bcast_header_cache = {};
-    std::uint64_t buf_physical_addr = 0;
-    int driver_id;  
     bool perform_harvesting_on_sdesc = false;
     bool use_ethernet_ordered_writes = true;
     bool use_ethernet_broadcast = true;

59ab3879f39764823e0e4944a17b2680c6c3970f
Bojan Rosko <brosko@tenstorrent.com>
2024-09-24 19:50:12 +0200

    some path printout to revert for testing

--- tests/test_utils/generate_cluster_desc.hpp
+++ tests/test_utils/generate_cluster_desc.hpp
@@ -14,8 +14,13 @@ namespace test_utils {
 
 inline std::string GetAbsPath(std::string path_){
     std::filesystem::path umd_root_relative = std::filesystem::relative(std::filesystem::path(__FILE__).parent_path().parent_path().parent_path(), "../");
+        std::cout << "during compilation path was " << std::filesystem::path(__FILE__).string() << std::endl;
+        std::cout << "umd_root_relative " << umd_root_relative.string() << std::endl;
     std::filesystem::path umd_root = std::filesystem::canonical(umd_root_relative);
+        std::cout << "umd_root " << umd_root.string() << std::endl;
     std::filesystem::path abs_path = umd_root / path_;
+        std::cout << "abs_path " << abs_path.string() << std::endl;
+        std::cout << "path_ " << path_ << std::endl;
     return abs_path.string();
 }
 
@@ -34,6 +39,7 @@ inline std::string GetClusterDescYAML(){
         }
         // Generates the cluster descriptor in the CWD
 
+        std::cout << "umd_path " << umd_path.string() << std::endl;
         std::filesystem::path eth_fpath = umd_path / "device/bin/silicon/x86/create-ethernet-map";
         std::string cmd = eth_fpath.string() + " " + cluster_path.string();
         int val = system(cmd.c_str());

2dc1d889b243e8355204b01754db1ad419e9ee4d
pjanevski <pjanevski@tenstorrent.com>
2024-10-01 13:57:22 +0000

    Address comments

--- ideal_device/io/tlb_manager.h
+++ ideal_device/io/tlb_manager.h
@@ -15,19 +15,22 @@ Questions:
 
 */
 
-#include "pci_device.h"
+#include "tt_device.h"
 #include "tlb_window.h"
 
 namespace tt::umd {
+
+typedef enum TLBType {
+    WC,
+    UC
+} TLBType;
 
 class TLBManager {
 
 public:
     virtual TLBWindow get_tlb_window(tt_xy_pair core) = 0;
     
-    virtual TLBWindow get_wc_tlb_window() = 0;
-
-    virtual TLBWindow get_uc_tlb_window() = 0;
+    virtual TLBWindow get_tlb_window(TLBType type) = 0;
 
     virtual void release_tlb_window(TLBWindow& tlb_window) = 0;
 
@@ -42,11 +45,6 @@ protected:
 
     static constexpr uint32_t wc_window_start = 0;
     static constexpr uint32_t wc_window_count = 0;
-
-    // dummy implementation
-    uint32_t map_core_to_tlb(tt_xy_pair core) {
-        return 0;
-    }
 }
 
 // This should represent how we do things today. We have whole BAR0/4 space
@@ -54,10 +52,10 @@ protected:
 class StandardTLBManager : public TLBManager {
 
 public:
-    StandardTLBManager(PCIDevice* pci_device) : pci_device(pci_device) {}
+    StandardTLBManager(TTDevice* tt_device) : tt_device(tt_device) {}
 
     TLBWindow get_tlb_window(tt_xy_pair core) override {
-        uint32_t tlb_index = map_core_to_tlb_index(core);
+        uint32_t tlb_index = tt_device->map_core_to_tlb_index(core);
         // This should be a WC window.
         if (tlb_index < wc_window_start || tlb_index >= uc_window_start) {
             // throw error.
@@ -66,10 +64,65 @@ public:
 
         return get_tlb_window(tlb_index);
     }
-    
+
+    TLBWindow get_tlb_window(TLBType type) override {
+        if (type == TLBType::WC) {
+            return get_wc_tlb_window();
+        } else if (type == TLBType::UC) {
+            return get_uc_tlb_window();
+        }
+    }
+
+    void release_tlb_window(TLBWindow& tlb_window) override {
+        uint32_t tlb_index = tlb_window.index;
+        if (tlb_index >= wc_window_start && tlb_index < uc_window_start) {
+            // Do we need to reprogram it?
+            wc_tlb_window_count[tlb_index - wc_window_start]--;
+        } else if (tlb_index >= uc_window_start) {
+            // Do we need to reprogram it?
+            uc_tlb_window_count[tlb_index - uc_window_start]--;
+        } else {
+            // throw error.
+        }
+    }
+
+    TLBWindow get_dram_tlb_widow(uint32_t dram_channel, uint32_t addr = 0) override {
+        // We need to get DRAM core from DRAM channel.
+        return get_tlb_window(tt_xy_pair(0, 0));
+    }
+
+    void release_dram_tlb_window(TLBWindow& tlb_window) override {
+        release_tlb_window(tlb_window);
+    }
+
+private:
+    TTDevice* tt_device;
+
+    // Do we allow reuse of WC TLB windows?
+    uint32_t wc_tlb_window_count[wc_window_count];
+    uint32_t uc_tlb_window_count[uc_window_count];
+
+
+    TLBWindow get_tlb_window(uint32_t tlb_index) {
+        if (!tt_device->pci_device->bar0_wc) {
+            throw std::runtime_error("No write-combined mapping for BAR0");
+        }
+
+        auto tlb_data = tt_device->pci_device->describe_tlb(tlb_index);
+
+        if (!tlb_data.has_value()) {
+            throw std::runtime_error("No TLB mapped to core " + target.str());
+        }
+
+        auto [tlb_offset, tlb_size] = tlb_data.value();
+        uint8_t* base = reinterpret_cast<uint8_t*>(tt_device->pci_device->bar0_wc);
+
+        return TLBWindow(base + tlb_offset, tlb_size);
+    }
+
     // Get unused WC TLB window.
     TLBWindow get_wc_tlb_window() override {
-        // Find unused UC TLB window.
+        // Find unused WC TLB window.
         for (uint32_t i = 0; i < wc_window_count; i++) {
             if (wc_tlb_window_count[i] == 0) {
                 wc_tlb_window_count[i]++;
@@ -89,53 +142,6 @@ public:
                 return get_tlb_window(uc_window_start + i);
             }
         }
-    }
-
-    void release_tlb_window(TLBWindow& tlb_window) override {
-        uint32_t tlb_index = tlb_window.index;
-        if (tlb_index >= wc_window_start && tlb_index < uc_window_start) {
-            // Do we need to reprogram it?
-            wc_tlb_window_count[tlb_index - wc_window_start]--;
-        } else if (tlb_index >= uc_window_start) {
-            // Do we need to reprogram it?
-            uc_tlb_window_count[tlb_index - uc_window_start]--;
-        } else {
-            // throw error.
-        }
-    }
-
-    TLBWindow get_dram_tlb_widow(uint32_t dram_channel, uint32_t addr = 0) override {
-        // We need to get DRAM core from DRAM channel.
-        return get_wc_tlb_window(tt_xy_pair(0, 0));
-    }
-
-    void release_dram_tlb_window(TLBWindow& tlb_window) override {
-        release_tlb_window(tlb_window);
-    }
-
-private:
-    PCIDevice* pci_device;
-
-    // Do we allow reuse of WC TLB windows?
-    uint32_t wc_tlb_window_count[wc_window_count];
-    uint32_t uc_tlb_window_count[uc_window_count];
-
-
-    TLBWindow get_tlb_window(uint32_t tlb_index) {
-        if (!dev->pci_device->bar0_wc) {
-            throw std::runtime_error("No write-combined mapping for BAR0");
-        }
-
-        auto tlb_data = pci_device->describe_tlb(tlb_index);
-
-        if (!tlb_data.has_value()) {
-            throw std::runtime_error("No TLB mapped to core " + target.str());
-        }
-
-        auto [tlb_offset, tlb_size] = tlb_data.value();
-        uint8_t* base = reinterpret_cast<uint8_t*>(pci_device->bar0_wc);
-
-        return TLBWindow(base + tlb_offset, tlb_size);
     }
 }
 
@@ -158,12 +164,8 @@ public:
     TLBWindow get_tlb_window(tt_xy_pair core) override {
         // kmd call.
     }
-    
-    TLBWindow get_wc_tlb_window() override {
-        // kmd call.
-    }
 
-    TLBWindow get_uc_tlb_window() override {
+    TLBWindow get_tlb_window(TLBType type) override {
         // kmd call.
     }
 

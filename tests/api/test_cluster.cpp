20a2f0635a4f63a8ccbf0840d276e9358cc68c12
Bojan Rosko <brosko@tenstorrent.com>
2024-10-14 15:28:45 +0200

    fix for grayskull

--- tests/api/test_cluster.cpp
+++ tests/api/test_cluster.cpp
@@ -24,10 +24,29 @@ using Cluster = tt_SiliconDevice;
 // This test should be one line only.
 TEST(ApiTest, OpenAllChips) {
 
+    // TODO: This should not be needed. And could be part of the cluster descriptor probably.
+    // Note that cluster descriptor holds logical ids of chips.
+    // Which are different than physical PCI ids, which are /dev/tenstorrent/N ones.
+    // You have to see if physical PCIe is GS before constructing a cluster descriptor.
+    std::vector<int> pci_device_ids = PCIDevice::enumerate_devices();
+    std::set<int> pci_device_ids_set(pci_device_ids.begin(), pci_device_ids.end());
+
+    // TODO: This should be removed from the API, the driver itself should do it.
+    std::string soc_path;
+    int physical_device_id = pci_device_ids[0];
+    // TODO: remove logical_device_id
+    PCIDevice pci_device (physical_device_id, 0);
+    tt::ARCH device_arch = pci_device.get_arch();
+
     // TODO: remove getting manually cluster descriptor from yaml.
     std::string yaml_path = test_utils::GetClusterDescYAML();
     // TODO: Remove the need to do this, allow default constructor to construct with all chips.
-    std::unique_ptr<tt_ClusterDescriptor> cluster_desc = tt_ClusterDescriptor::create_from_yaml(yaml_path);
+    std::unique_ptr<tt_ClusterDescriptor> cluster_desc;
+    if (device_arch == tt::ARCH::GRAYSKULL) {
+        cluster_desc = tt_ClusterDescriptor::create_for_grayskull_cluster(pci_device_ids_set, pci_device_ids);
+    } else {
+        cluster_desc = tt_ClusterDescriptor::create_from_yaml(yaml_path);
+    }
     std::unordered_set<int> detected_num_chips = cluster_desc->get_all_chips();
 
     // TODO: make these unordered vs
@@ -36,12 +55,6 @@ TEST(ApiTest, OpenAllChips) {
         detected_num_chips_set.insert(chip);
     }
 
-    // TODO: This should be removed from the API, the driver itself should do it.
-    std::string soc_path;
-    int logical_device_id = *detected_num_chips.begin();
-    int physical_device_id = cluster_desc->get_chips_with_mmio()[logical_device_id];
-    PCIDevice pci_device (physical_device_id, logical_device_id);
-    tt::ARCH device_arch = pci_device.get_arch();
     
     // TODO: This would be incorporated inside SocDescriptor.
     if (device_arch == tt::ARCH::GRAYSKULL) {

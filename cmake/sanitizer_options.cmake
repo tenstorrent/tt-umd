a92227d417ac372b7655f1f9bac8ce74463764d0
blozano-tt <blozano@tenstorrent.com>
2024-10-18 08:38:14 -0700

    #0: Format cmake/sanitizer_options.cmake

--- cmake/sanitizer_options.cmake
+++ cmake/sanitizer_options.cmake
@@ -1,36 +1,36 @@
 include_guard(GLOBAL)
 
-    option(ENABLE_ASAN "Enable build with AddressSanitizer" OFF)
-    message(STATUS "Build with ASAN: ${ENABLE_ASAN}")
+option(ENABLE_ASAN "Enable build with AddressSanitizer" OFF)
+message(STATUS "Build with ASAN: ${ENABLE_ASAN}")
 
-    set(SANITIZER_ENABLED ${ENABLE_ASAN})
+set(SANITIZER_ENABLED ${ENABLE_ASAN})
 
-    option(ENABLE_MSAN "Enable build with MemorySanitizer" OFF)
-    message(STATUS "Build with MSAN: ${ENABLE_MSAN}")
+option(ENABLE_MSAN "Enable build with MemorySanitizer" OFF)
+message(STATUS "Build with MSAN: ${ENABLE_MSAN}")
 
-    if(SANITIZER_ENABLED AND ENABLE_MSAN)
-        message(FATAL_ERROR "Multiple sanitizers are not supported")
-    elseif(ENABLE_MSAN)
-        set(SANITIZER_ENABLED ${ENABLE_MSAN})
-    endif()
+if(SANITIZER_ENABLED AND ENABLE_MSAN)
+    message(FATAL_ERROR "Multiple sanitizers are not supported")
+elseif(ENABLE_MSAN)
+    set(SANITIZER_ENABLED ${ENABLE_MSAN})
+endif()
 
-    option(ENABLE_TSAN "Enable build with ThreadSanitizer" OFF)
-    message(STATUS "Build with TSAN: ${ENABLE_TSAN}")
+option(ENABLE_TSAN "Enable build with ThreadSanitizer" OFF)
+message(STATUS "Build with TSAN: ${ENABLE_TSAN}")
 
-    if(SANITIZER_ENABLED AND ENABLE_TSAN)
-        message(FATAL_ERROR "Multiple sanitizers are not supported")
-    elseif(ENABLE_TSAN)
-        set(SANITIZER_ENABLED ${ENABLE_TSAN})
-    endif()
+if(SANITIZER_ENABLED AND ENABLE_TSAN)
+    message(FATAL_ERROR "Multiple sanitizers are not supported")
+elseif(ENABLE_TSAN)
+    set(SANITIZER_ENABLED ${ENABLE_TSAN})
+endif()
 
-    option(ENABLE_UBSAN "Enable build with UndefinedBehaviorSanitizer" OFF)
-    message(STATUS "Build with UBSAN: ${ENABLE_UBSAN}")
+option(ENABLE_UBSAN "Enable build with UndefinedBehaviorSanitizer" OFF)
+message(STATUS "Build with UBSAN: ${ENABLE_UBSAN}")
 
-    if(SANITIZER_ENABLED AND ENABLE_UBSAN)
-        message(FATAL_ERROR "Multiple sanitizers are not supported")
-    endif()
+if(SANITIZER_ENABLED AND ENABLE_UBSAN)
+    message(FATAL_ERROR "Multiple sanitizers are not supported")
+endif()
 
-    unset(SANITIZER_ENABLED)
+unset(SANITIZER_ENABLED)
 
 add_compile_options(
     $<$<BOOL:${ENABLE_ASAN}>:-fsanitize=address>
@@ -45,8 +45,8 @@ add_link_options(
     $<$<BOOL:${ENABLE_UBSAN}>:-fsanitize=undefined>
 )
 
-    add_library(compiler_flags INTERFACE)
+add_library(compiler_flags INTERFACE)
 
-    add_library(linker_flags INTERFACE)
+add_library(linker_flags INTERFACE)
 
-    target_link_libraries(compiler_flags INTERFACE linker_flags)
+target_link_libraries(compiler_flags INTERFACE linker_flags)

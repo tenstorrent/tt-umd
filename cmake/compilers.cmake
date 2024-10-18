419693d49feddc178766066d776082448102891d
blozano-tt <blozano@tenstorrent.com>
2024-10-18 07:45:40 +0000

    Revert "CMake Cleanup (#181)"
    This reverts commit 6b50158bd752811e670ff11c4fd74b3de6d32120.

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,32 +1,21 @@
-include_guard(GLOBAL)
-
 function(FIND_AND_SET_CLANG17)
     find_program(CLANGPP_17 clang++-17)
     find_program(CLANG_17 clang-17)
 
     if(NOT CLANGPP_17 OR NOT CLANG_17)
-        message(
-            FATAL_ERROR
-            "Clang-17 not found. Make sure you have clang-17 and clang++-17 installed and in your PATH"
-        )
+        message(FATAL_ERROR "Clang-17 not found. Make sure you have clang-17 and clang++-17 installed and in your PATH")
     endif()
 
     set(CMAKE_CXX_COMPILER "${CLANGPP_17}" PARENT_SCOPE)
     set(CMAKE_C_COMPILER "${CLANG_17}" PARENT_SCOPE)
 endfunction()
 
+
 function(CHECK_COMPILERS)
     message(STATUS "Checking compilers")
 
     if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-        if(
-            CMAKE_CXX_COMPILER_VERSION
-                VERSION_LESS
-                "17.0.0"
-            OR CMAKE_CXX_COMPILER_VERSION
-                GREATER_EQUAL
-                "18.0.0"
-        )
+        if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "17.0.0" OR CMAKE_CXX_COMPILER_VERSION GREATER_EQUAL "18.0.0")
             message(WARNING "Only Clang-17 is tested right now")
         endif()
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
@@ -36,57 +25,22 @@ function(CHECK_COMPILERS)
             message(WARNING "Only GCC-12 is tested right now")
         endif()
     else()
-        message(
-            FATAL_ERROR
-            "Unsupported compiler: ${CMAKE_CXX_COMPILER_ID} ! Only Clang and GCC are supported"
-        )
+        message(FATAL_ERROR "Unsupported compiler: ${CMAKE_CXX_COMPILER_ID} ! Only Clang and GCC are supported")
     endif()
 endfunction()
+
 
 function(ADJUST_COMPILER_WARNINGS)
     if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-        target_compile_options(
-            compiler_warnings
-            INTERFACE
-                -Wsometimes-uninitialized
-                -Wno-c++11-narrowing
-                -Wno-error=local-type-template-args
-                -Wno-delete-non-abstract-non-virtual-dtor
-                -Wno-c99-designator
-                -Wno-shift-op-parentheses
-                -Wno-non-c-typedef-for-linkage
-                -Wno-deprecated-this-capture
-                -Wno-deprecated-volatile
-                -Wno-deprecated-builtins
-                -Wno-deprecated-declarations
+        target_compile_options(compiler_warnings INTERFACE
+            -Wsometimes-uninitialized -Wno-c++11-narrowing -Wno-error=local-type-template-args
+            -Wno-delete-non-abstract-non-virtual-dtor -Wno-c99-designator -Wno-shift-op-parentheses -Wno-non-c-typedef-for-linkage
+            -Wno-deprecated-this-capture -Wno-deprecated-volatile -Wno-deprecated-builtins -Wno-deprecated-declarations
         )
     else() # GCC-12 or higher
-        target_compile_options(
-            compiler_warnings
-            INTERFACE
-                -Wno-deprecated
-                -Wno-attributes
-                -Wno-stringop-overread
-                -Wno-stringop-overflow
-                -Wno-maybe-uninitialized
-                -Wno-missing-requires
-                -Wno-narrowing
-                -Wno-non-template-friend
-                -Wno-error=non-template-friend
+        target_compile_options(compiler_warnings INTERFACE
+            -Wno-deprecated -Wno-attributes -Wno-stringop-overread -Wno-stringop-overflow -Wno-maybe-uninitialized -Wno-missing-requires
+            -Wno-narrowing -Wno-non-template-friend -Wno-error=non-template-friend
         )
     endif()
 endfunction()
-
-if(DEFINED ENV{CMAKE_C_COMPILER} AND DEFINED ENV{CMAKE_CXX_COMPILER})
-    message(STATUS "Setting C and C++ compiler from environment variables")
-    set(CMAKE_C_COMPILER $ENV{CMAKE_C_COMPILER})
-    set(CMAKE_CXX_COMPILER $ENV{CMAKE_CXX_COMPILER})
-endif()
-
-if(CMAKE_CXX_COMPILER AND CMAKE_C_COMPILER)
-    message(STATUS "Using specifed C++ compiler: ${CMAKE_CXX_COMPILER}")
-    message(STATUS "Using specifed C compiler: ${CMAKE_C_COMPILER}")
-else()
-    message(STATUS "No C or C++ compiler specified, defaulting to Clang-17")
-    FIND_AND_SET_CLANG17()
-endif()

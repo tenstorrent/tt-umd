731a86f61de405acac584d22752681efbf25c6a3
blozano-tt <blozano@tenstorrent.com>
2024-11-05 00:32:54 +0000

    Fix CI

--- tests/blackhole/test_silicon_driver_bh.cpp
+++ tests/blackhole/test_silicon_driver_bh.cpp
@@ -6,8 +6,6 @@
 #include <tt_device.h>
 #include "eth_l1_address_map.h"
 #include "l1_address_map.h"
-#include "eth_l1_address_map.h"
-#include "eth_interface.h"
 #include "host_mem_address_map.h"
 #include <thread>
 #include <memory>
@@ -16,6 +14,8 @@
 #include "device/tt_cluster_descriptor.h"
 #include "tests/test_utils/generate_cluster_desc.hpp"
 #include "tests/test_utils/device_test_utils.hpp"
+
+#include "noc/noc_parameters.h"
 
 void set_params_for_remote_txn(tt_SiliconDevice& device) {
     // Populate address map and NOC parameters that the driver needs for remote transactions
--- tests/wormhole/test_silicon_driver_wh.cpp
+++ tests/wormhole/test_silicon_driver_wh.cpp
@@ -9,14 +9,14 @@
 #include "tt_device.h"
 #include "eth_l1_address_map.h"
 #include "l1_address_map.h"
-#include "eth_l1_address_map.h"
-#include "eth_interface.h"
 #include "host_mem_address_map.h"
 
 #include "device/tt_cluster_descriptor.h"
 #include "device/wormhole/wormhole_implementation.h"
 #include "tests/test_utils/generate_cluster_desc.hpp"
 #include "tests/test_utils/device_test_utils.hpp"
+
+#include "noc/noc_parameters.h"
 
 inline void fill_with_random_bytes(uint8_t* data, size_t n)
 {

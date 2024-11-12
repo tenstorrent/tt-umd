f6b2e585d3f37a8f726ea749033915c3abc4a327
pjanevski <pjanevski@tenstorrent.com>
2024-11-12 14:56:01 +0000

    Debug CI

--- common/utils.hpp
+++ common/utils.hpp
@@ -15,11 +15,13 @@ namespace tt::umd::utils {
 std::string get_abs_path(std::string path) {
     // Note that __FILE__ might be resolved at compile time to an absolute or relative address, depending on the
     // compiler.
-    return std::filesystem::absolute(path).string();
-    // std::filesystem::path current_file_path = std::filesystem::absolute(__FILE__);
-    // std::filesystem::path umd_root = current_file_path.parent_path().parent_path();
-    // std::filesystem::path abs_path = umd_root / path;
-    // return abs_path.string();
+    std::filesystem::path current_file_path = std::filesystem::absolute(__FILE__);
+    std::cout << "Current file path: " << current_file_path << std::endl;
+    std::filesystem::path umd_root = current_file_path.parent_path().parent_path();
+    std::cout << "UMD root: " << umd_root << std::endl;
+    std::filesystem::path abs_path = umd_root / path;
+    std::cout << "Absolute path: " << abs_path << std::endl;
+    return abs_path.string();
 }
 
 }  // namespace tt::umd::utils

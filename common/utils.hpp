580d6823dd5194685c8a725277fa6259501a3757
pjanevski <pjanevski@tenstorrent.com>
2024-11-12 14:48:33 +0000

    Fix abs path

--- common/utils.hpp
+++ common/utils.hpp
@@ -15,10 +15,11 @@ namespace tt::umd::utils {
 std::string get_abs_path(std::string path) {
     // Note that __FILE__ might be resolved at compile time to an absolute or relative address, depending on the
     // compiler.
-    std::filesystem::path current_file_path = std::filesystem::absolute(__FILE__);
-    std::filesystem::path umd_root = current_file_path.parent_path().parent_path();
-    std::filesystem::path abs_path = umd_root / path;
-    return abs_path.string();
+    return std::filesystem::absolute(path).string();
+    // std::filesystem::path current_file_path = std::filesystem::absolute(__FILE__);
+    // std::filesystem::path umd_root = current_file_path.parent_path().parent_path();
+    // std::filesystem::path abs_path = umd_root / path;
+    // return abs_path.string();
 }
 
 }  // namespace tt::umd::utils

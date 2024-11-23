8bda89581f620629c32e1189183d2b272558fb66
Bryan Wilder Field Lozano <blozano@tenstorrent.com>
2024-11-22 17:01:06 -0800

    Make uv and nng private and static (#323)

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -56,7 +56,6 @@ function(fetch_dependencies)
         GITHUB_REPOSITORY nanomsg/nng
         GIT_TAG v1.8.0
         OPTIONS
-            "BUILD_SHARED_LIBS ON"
             "NNG_TESTS OFF"
             "NNG_TOOLS OFF"
     )
@@ -97,7 +96,14 @@ function(fetch_dependencies)
     ############################################################################################################################
     # libuv (for process management)
     ############################################################################################################################
-    CPMAddPackage(NAME libuv GITHUB_REPOSITORY libuv/libuv GIT_TAG v1.48.0 OPTIONS "LIBUV_BUILD_TESTS OFF")
+    CPMAddPackage(
+        NAME libuv
+        GITHUB_REPOSITORY libuv/libuv
+        GIT_TAG v1.48.0
+        OPTIONS
+            "LIBUV_BUILD_TESTS OFF"
+            "LIBUV_BUILD_SHARED OFF"
+    )
 
     ############################################################################################################################
     # fmt : https://github.com/fmtlib/fmt

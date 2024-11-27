c277239bca32f7f506c37d77824bc568847d772d
Bojan Rosko <brosko@tenstorrent.com>
2024-11-27 15:24:12 +0000

    still wip

---
+++ device/api/umd/device/chip/local_chip.h
@@ -0,0 +1,16 @@
+/*
+ * SPDX-FileCopyrightText: (c) 2024 Tenstorrent Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include "umd/device/chip/chip.h"
+
+namespace tt::umd {
+class LocalChip : public Chip {
+public:
+    LocalChip(chip_id_t chip_id, tt_SocDescriptor soc_descriptor);
+};
+}  // namespace tt::umd

44bec365ab6f4d8921c2320e4889d6d45c36aa9f
pjanevski <pjanevski@tenstorrent.com>
2024-12-05 11:15:29 +0000

    Coordinate manager maps redesign

--- device/grayskull/grayskull_coordinate_manager.cpp
+++ device/grayskull/grayskull_coordinate_manager.cpp
@@ -43,13 +43,14 @@ GrayskullCoordinateManager::GrayskullCoordinateManager(
 void GrayskullCoordinateManager::fill_eth_logical_to_translated() {
     for (size_t x = 0; x < eth_grid_size.x; x++) {
         for (size_t y = 0; y < eth_grid_size.y; y++) {
-            const CoreCoord physical_coord = eth_logical_to_physical[{x, y}];
-            const size_t translated_x = physical_coord.x;
-            const size_t translated_y = physical_coord.y;
-            eth_logical_to_translated[{x, y}] =
-                CoreCoord(translated_x, translated_y, CoreType::ETH, CoordSystem::TRANSLATED);
-            eth_translated_to_logical[{translated_x, translated_y}] =
-                CoreCoord(x, y, CoreType::ETH, CoordSystem::LOGICAL);
+            CoreCoord logical_coord = CoreCoord(x, y, CoreType::ETH, CoordSystem::LOGICAL);
+            const tt_xy_pair physical_pair = to_physical_map[logical_coord];
+            const size_t translated_x = physical_pair.x;
+            const size_t translated_y = physical_pair.y;
+
+            CoreCoord translated_coord = CoreCoord(translated_x, translated_y, CoreType::ETH, CoordSystem::TRANSLATED);
+            to_physical_map[translated_coord] = physical_pair;
+            from_physical_map[{{physical_pair.x, physical_pair.y}, CoordSystem::TRANSLATED}] = translated_coord;
         }
     }
 }

932dd19b778192ab3fc6b030c21fe89841a1f5c9
Bojan Rosko <brosko@tenstorrent.com>
2024-12-09 12:44:39 +0000

    gcc errors

--- device/coordinate_manager.cpp
+++ device/coordinate_manager.cpp
@@ -163,6 +163,9 @@ CoreCoord CoordinateManager::to_physical(const CoreCoord core_coord) {
             auto& logical_mapping = get_logical_to_physical(core_coord.core_type);
             return logical_mapping[{core_coord.x, core_coord.y}];
         }
+        default:
+            throw std::runtime_error(
+                "Unexpected CoordSystem value " + std::to_string((uint8_t)core_coord.coord_system));
     }
 }
 
@@ -177,6 +180,9 @@ CoreCoord CoordinateManager::to_virtual(const CoreCoord core_coord) {
             auto& logical_mapping = get_logical_to_virtual(core_coord.core_type);
             return logical_mapping[{core_coord.x, core_coord.y}];
         }
+        default:
+            throw std::runtime_error(
+                "Unexpected CoordSystem value " + std::to_string((uint8_t)core_coord.coord_system));
     }
 }
 
@@ -196,6 +202,9 @@ CoreCoord CoordinateManager::to_logical(const CoreCoord core_coord) {
             auto& translated_mapping = get_translated_to_logical(core_coord.core_type);
             return translated_mapping[{core_coord.x, core_coord.y}];
         }
+        default:
+            throw std::runtime_error(
+                "Unexpected CoordSystem value " + std::to_string((uint8_t)core_coord.coord_system));
     }
 }
 
@@ -210,6 +219,9 @@ CoreCoord CoordinateManager::to_translated(const CoreCoord core_coord) {
             auto& logical_mapping = get_logical_to_translated(core_coord.core_type);
             return logical_mapping[{core_coord.x, core_coord.y}];
         }
+        default:
+            throw std::runtime_error(
+                "Unexpected CoordSystem value " + std::to_string((uint8_t)core_coord.coord_system));
     }
 }
 
@@ -223,6 +235,9 @@ CoreCoord CoordinateManager::to(const CoreCoord core_coord, const CoordSystem co
             return to_virtual(core_coord);
         case CoordSystem::TRANSLATED:
             return to_translated(core_coord);
+        default:
+            throw std::runtime_error(
+                "Unexpected CoordSystem value " + std::to_string((uint8_t)core_coord.coord_system));
     }
 }
 
@@ -457,6 +472,8 @@ std::shared_ptr<CoordinateManager> CoordinateManager::create_coordinate_manager(
                 tt::umd::blackhole::PCIE_CORES);
         case tt::ARCH::Invalid:
             throw std::runtime_error("Invalid architecture for creating coordinate manager");
+        default:
+            throw std::runtime_error("Unexpected ARCH value " + std::to_string((int)arch));
     }
 }
 

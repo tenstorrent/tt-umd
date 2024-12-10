d575d90ca09c98bcdad68ef1b9ccee3d0ee32972
Bojan Roško <156314064+broskoTT@users.noreply.github.com>
2024-12-10 14:24:39 +0100

    Disable soft reset tests (#388)
    ### Issue
    Related to #362 
    
    ### Description
    These tests have been failing intermittently. I've started investigating
    them, but they are producing false negatives and interfering with other
    work on UMD.
    They don't point to something new failing on UMD, rather they haven't
    been setup properly (they have been failing since added).
    
    ### List of the changes
    - Comment out soft assert tests.
    
    ### Testing
    not applicable
    
    ### API Changes
    There are no API changes in this PR.

--- tests/api/test_chip.cpp
+++ tests/api/test_chip.cpp
@@ -111,71 +111,72 @@ TEST(ApiChipTest, SimpleAPIShowcase) {
     umd_cluster->get_num_host_channels(chip_id);
 }
 
-// This tests puts a specific core into reset and then deasserts it using default deassert value
-// It reads back the risc reset reg to validate
-TEST(ApiChipTest, DeassertRiscResetOnCore) {
-    std::unique_ptr<Cluster> umd_cluster = get_cluster();
+// TODO: Re-enable once we debug why it doesn't work #362
+// // This tests puts a specific core into reset and then deasserts it using default deassert value
+// // It reads back the risc reset reg to validate
+// TEST(ApiChipTest, DeassertRiscResetOnCore) {
+//     std::unique_ptr<Cluster> umd_cluster = get_cluster();
 
-    if (umd_cluster == nullptr || umd_cluster->get_all_chips_in_cluster().empty()) {
-        GTEST_SKIP() << "No chips present on the system. Skipping test.";
-    }
+//     if (umd_cluster == nullptr || umd_cluster->get_all_chips_in_cluster().empty()) {
+//         GTEST_SKIP() << "No chips present on the system. Skipping test.";
+//     }
 
-    tt_cxy_pair chip_core_coord = get_tensix_chip_core_coord(umd_cluster);
+//     tt_cxy_pair chip_core_coord = get_tensix_chip_core_coord(umd_cluster);
 
-    umd_cluster->assert_risc_reset_at_core(chip_core_coord);
-    umd_cluster->l1_membar(chip_core_coord.chip, "LARGE_WRITE_TLB");
-    umd_cluster->deassert_risc_reset_at_core(chip_core_coord);
-    umd_cluster->l1_membar(chip_core_coord.chip, "LARGE_WRITE_TLB");
+//     umd_cluster->assert_risc_reset_at_core(chip_core_coord);
+//     umd_cluster->l1_membar(chip_core_coord.chip, "LARGE_WRITE_TLB");
+//     umd_cluster->deassert_risc_reset_at_core(chip_core_coord);
+//     umd_cluster->l1_membar(chip_core_coord.chip, "LARGE_WRITE_TLB");
 
-    uint32_t soft_reset_reg_addr = 0xFFB121B0;
-    uint32_t expected_risc_reset_val = static_cast<uint32_t>(TENSIX_DEASSERT_SOFT_RESET);
-    uint32_t risc_reset_val;
-    umd_cluster->read_from_device(&risc_reset_val, chip_core_coord, soft_reset_reg_addr, sizeof(uint32_t), "REG_TLB");
-    EXPECT_EQ(expected_risc_reset_val, risc_reset_val);
-}
+//     uint32_t soft_reset_reg_addr = 0xFFB121B0;
+//     uint32_t expected_risc_reset_val = static_cast<uint32_t>(TENSIX_DEASSERT_SOFT_RESET);
+//     uint32_t risc_reset_val;
+//     umd_cluster->read_from_device(&risc_reset_val, chip_core_coord, soft_reset_reg_addr, sizeof(uint32_t),
+//     "REG_TLB"); EXPECT_EQ(expected_risc_reset_val, risc_reset_val);
+// }
 
-// This tests puts a specific core into reset and then specifies a legal deassert value
-// It reads back the risc reset reg to validate
-TEST(ApiChipTest, SpecifyLegalDeassertRiscResetOnCore) {
-    std::unique_ptr<Cluster> umd_cluster = get_cluster();
+// // This tests puts a specific core into reset and then specifies a legal deassert value
+// // It reads back the risc reset reg to validate
+// TEST(ApiChipTest, SpecifyLegalDeassertRiscResetOnCore) {
+//     std::unique_ptr<Cluster> umd_cluster = get_cluster();
 
-    if (umd_cluster == nullptr || umd_cluster->get_all_chips_in_cluster().empty()) {
-        GTEST_SKIP() << "No chips present on the system. Skipping test.";
-    }
+//     if (umd_cluster == nullptr || umd_cluster->get_all_chips_in_cluster().empty()) {
+//         GTEST_SKIP() << "No chips present on the system. Skipping test.";
+//     }
 
-    tt_cxy_pair chip_core_coord = get_tensix_chip_core_coord(umd_cluster);
+//     tt_cxy_pair chip_core_coord = get_tensix_chip_core_coord(umd_cluster);
 
-    umd_cluster->assert_risc_reset_at_core(chip_core_coord);
-    TensixSoftResetOptions deassert_val = ALL_TRISC_SOFT_RESET | TensixSoftResetOptions::STAGGERED_START;
-    umd_cluster->deassert_risc_reset_at_core(chip_core_coord, deassert_val);
-    umd_cluster->l1_membar(chip_core_coord.chip, "LARGE_WRITE_TLB");
+//     umd_cluster->assert_risc_reset_at_core(chip_core_coord);
+//     TensixSoftResetOptions deassert_val = ALL_TRISC_SOFT_RESET | TensixSoftResetOptions::STAGGERED_START;
+//     umd_cluster->deassert_risc_reset_at_core(chip_core_coord, deassert_val);
+//     umd_cluster->l1_membar(chip_core_coord.chip, "LARGE_WRITE_TLB");
 
-    uint32_t soft_reset_reg_addr = 0xFFB121B0;
-    uint32_t risc_reset_val;
-    umd_cluster->read_from_device(&risc_reset_val, chip_core_coord, soft_reset_reg_addr, sizeof(uint32_t), "REG_TLB");
-    EXPECT_EQ(static_cast<uint32_t>(deassert_val), risc_reset_val);
-}
+//     uint32_t soft_reset_reg_addr = 0xFFB121B0;
+//     uint32_t risc_reset_val;
+//     umd_cluster->read_from_device(&risc_reset_val, chip_core_coord, soft_reset_reg_addr, sizeof(uint32_t),
+//     "REG_TLB"); EXPECT_EQ(static_cast<uint32_t>(deassert_val), risc_reset_val);
+// }
 
-// // This tests puts a specific core into reset and then specifies an illegal deassert value
-// // It reads back the risc reset reg to validate that reset reg is in a legal state
-TEST(ApiChipTest, SpecifyIllegalDeassertRiscResetOnCore) {
-    std::unique_ptr<Cluster> umd_cluster = get_cluster();
+// // // This tests puts a specific core into reset and then specifies an illegal deassert value
+// // // It reads back the risc reset reg to validate that reset reg is in a legal state
+// TEST(ApiChipTest, SpecifyIllegalDeassertRiscResetOnCore) {
+//     std::unique_ptr<Cluster> umd_cluster = get_cluster();
 
-    if (umd_cluster == nullptr || umd_cluster->get_all_chips_in_cluster().empty()) {
-        GTEST_SKIP() << "No chips present on the system. Skipping test.";
-    }
+//     if (umd_cluster == nullptr || umd_cluster->get_all_chips_in_cluster().empty()) {
+//         GTEST_SKIP() << "No chips present on the system. Skipping test.";
+//     }
 
-    tt_cxy_pair chip_core_coord = get_tensix_chip_core_coord(umd_cluster);
+//     tt_cxy_pair chip_core_coord = get_tensix_chip_core_coord(umd_cluster);
 
-    umd_cluster->assert_risc_reset_at_core(chip_core_coord);
+//     umd_cluster->assert_risc_reset_at_core(chip_core_coord);
 
-    TensixSoftResetOptions deassert_val = static_cast<TensixSoftResetOptions>(0xDEADBEEF);
-    umd_cluster->deassert_risc_reset_at_core(chip_core_coord, deassert_val);
-    umd_cluster->l1_membar(chip_core_coord.chip, "LARGE_WRITE_TLB");
+//     TensixSoftResetOptions deassert_val = static_cast<TensixSoftResetOptions>(0xDEADBEEF);
+//     umd_cluster->deassert_risc_reset_at_core(chip_core_coord, deassert_val);
+//     umd_cluster->l1_membar(chip_core_coord.chip, "LARGE_WRITE_TLB");
 
-    uint32_t soft_reset_reg_addr = 0xFFB121B0;
-    uint32_t risc_reset_val;
-    umd_cluster->read_from_device(&risc_reset_val, chip_core_coord, soft_reset_reg_addr, sizeof(uint32_t), "REG_TLB");
-    uint32_t expected_deassert_val = static_cast<uint32_t>(deassert_val & ALL_TENSIX_SOFT_RESET);
-    EXPECT_EQ(risc_reset_val, expected_deassert_val);
-}
+//     uint32_t soft_reset_reg_addr = 0xFFB121B0;
+//     uint32_t risc_reset_val;
+//     umd_cluster->read_from_device(&risc_reset_val, chip_core_coord, soft_reset_reg_addr, sizeof(uint32_t),
+//     "REG_TLB"); uint32_t expected_deassert_val = static_cast<uint32_t>(deassert_val & ALL_TENSIX_SOFT_RESET);
+//     EXPECT_EQ(risc_reset_val, expected_deassert_val);
+// }

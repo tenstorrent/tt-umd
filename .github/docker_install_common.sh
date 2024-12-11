9ab40e76072660a59a5bf08d11e82cd46cd85d3c
Bojan Rosko <brosko@tenstorrent.com>
2024-12-11 19:53:04 +0000

    g++-12

--- .github/docker_install_common.sh
+++ .github/docker_install_common.sh
@@ -18,7 +18,7 @@ apt-get update && apt-get install -y \
 # gcc-12 should be available only for ubuntu 22 and not 20
 if apt-cache show gcc-12 > /dev/null 2>&1; then
     echo "gcc-12 is available. Installing..."
-    apt-get install -y gcc-12
+    apt-get install -y gcc-12 g++-12
 else
     echo "gcc-12 is not available in the repository."
 fi

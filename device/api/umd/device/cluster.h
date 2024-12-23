28a483b8c22062bf5c8d932ebc6308b1e316fb91
Bojan Roško <156314064+broskoTT@users.noreply.github.com>
2024-12-23 09:08:27 +0100

    Remove describe_tlb function (#419)
    ### Issue
    Related to #417 
    
    ### Description
    Seems like we've had a redundant get_tlb_configuration and describe_tlb
    functions. I also noted that sometimes we downcasted 64bit address to
    32bit, so this should solve those problems if they were even present.
    
    ### List of the changes
    - Removed describe_tlb everywhere
    - Changed describe_tlb calls with get_tlb_configuration
    - API now returns tlb_configuration type.
    
    ### Testing
    Existing CI tests
    
    ### API Changes
    This PR has API changes. I've left old API in place so that this change
    is not breaking, but is marked as deprecated.

--- device/api/umd/device/cluster.h
+++ device/api/umd/device/cluster.h
@@ -658,6 +658,10 @@ public:
      */
     std::optional<std::tuple<uint32_t, uint32_t>> get_tlb_data_from_target(const tt_cxy_pair& target);
     /**
+     * Returns a struct with the TLB configuration, or throws an exception if the target does not have a static TLB.
+     */
+    tlb_configuration get_tlb_configuration(const tt_cxy_pair& target);
+    /**
      * Provide fast write access to a statically-mapped TLB.
      * It is the caller's responsibility to ensure that
      * - the target has a static TLB mapping configured.
@@ -707,6 +711,7 @@ public:
         const std::string& fallback_tlb);
     std::optional<std::tuple<uint32_t, uint32_t>> get_tlb_data_from_target(
         const chip_id_t chip, const tt::umd::CoreCoord core);
+    tlb_configuration get_tlb_configuration(const chip_id_t chip, const tt::umd::CoreCoord core);
     tt::Writer get_static_tlb_writer(const chip_id_t chip, const tt::umd::CoreCoord target);
     virtual void configure_active_ethernet_cores_for_mmio_device(
         const std::unordered_set<tt::umd::CoreCoord>& active_eth_cores_per_chip, chip_id_t mmio_chip);

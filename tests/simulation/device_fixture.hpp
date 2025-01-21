78ccfaf823e38088cda21363dcc22431cee05b17
Vuk Jovanovic <vjovanovic@tenstorrent.com>
2025-01-21 11:11:22 +0000

    Fixing formatting...

--- tests/simulation/device_fixture.hpp
+++ tests/simulation/device_fixture.hpp
@@ -20,7 +20,9 @@ protected:
         // default_params and yaml path are both dummy and won't change test behavior
         tt_device_params default_params;
         const char* simulator_path = getenv("TT_UMD_SIMULATOR");
-        ASSERT_TRUE(simulator_path != nullptr, "You need to define TT_UMD_SIMULATOR that will point to simulator path. eg. build/versim-wormhole-b0");
+        ASSERT_TRUE(
+            simulator_path != nullptr,
+            "You need to define TT_UMD_SIMULATOR that will point to simulator path. eg. build/versim-wormhole-b0");
         device = std::make_unique<tt_SimulationDevice>(simulator_path);
         device->start_device(default_params);
     }

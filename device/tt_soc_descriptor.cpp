a57e9cf7cb7c07134d9de4c1e2e6d8a8b989a397
Bojan Rosko <brosko@tenstorrent.com>
2025-01-22 15:47:22 +0000

    minor

--- device/tt_soc_descriptor.cpp
+++ device/tt_soc_descriptor.cpp
@@ -225,6 +225,11 @@ tt::umd::CoreCoord tt_SocDescriptor::translate_coord_to(
     return coordinate_manager->translate_coord_to(core_coord, coord_system);
 }
 
+tt::umd::CoreCoord tt_SocDescriptor::translate_coord_to(
+    const tt_xy_pair core_location, const CoordSystem input_coord_system, const CoordSystem target_coord_system) const {
+    return coordinate_manager->translate_coord_to(core_location, input_coord_system, target_coord_system);
+}
+
 tt_SocDescriptor::tt_SocDescriptor(
     std::string device_descriptor_path,
     const bool noc_translation_enabled,

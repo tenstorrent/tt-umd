b15daee61d98db678b3131879da717865f8713f6
pjanevski <pjanevski@tenstorrent.com>
2025-03-03 17:57:51 +0000

    Implemenet TLB management through KMD

---
+++ device/tt_device/tlb_allocator.cpp
@@ -0,0 +1,17 @@
+/*
+ * SPDX-FileCopyrightText: (c) 2025 Tenstorrent Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "umd/device/tt_device/tlb_allocator.h"
+
+namespace tt::umd {
+
+TlbAllocator::TlbAllocator(PCIDevice* pci_device) : pci_device(pci_device) {}
+
+std::unique_ptr<TlbWindow> TlbAllocator::get_tlb(const uint64_t size, const tenstorrent_noc_tlb_config config) {
+    return std::make_unique<TlbWindow>(std::make_unique<TlbHandle>(pci_device, size, config));
+}
+
+}  // namespace tt::umd

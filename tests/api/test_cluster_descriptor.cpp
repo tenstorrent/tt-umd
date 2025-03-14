addd5e193cf8e8ecde78cd8d447a408cf4d5d31c
Bojan Roško <156314064+broskoTT@users.noreply.github.com>
2025-03-14 12:42:16 +0100

    Print cluster descriptor in contents (#611)
    ### Issue
    No issue, but inspired by #610 
    
    ### Description
    Adding cluster descriptor output in our tests, just for easier
    debugging.
    
    ### List of the changes
    - Print serialized generated cluster descriptor in case of BH and UBB
    - Print cluster descriptor generated by CEM in other cases
    - Changed the order so that API tests are executed first
    
    ### Testing
    - CI on this PR
    
    ### API Changes
    There are no API changes in this PR.

--- tests/api/test_cluster_descriptor.cpp
+++ tests/api/test_cluster_descriptor.cpp
@@ -4,6 +4,7 @@
 
 #include <gtest/gtest.h>
 
+#include <fstream>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -201,4 +202,30 @@ TEST(ApiClusterDescriptorTest, EthernetConnectivity) {
         }
         std::cout << std::endl;
     }
+}
+
+TEST(ApiClusterDescriptorTest, PrintClusterDescriptor) {
+    std::vector<int> pci_device_ids = PCIDevice::enumerate_devices();
+    if (pci_device_ids.size() == 0) {
+        GTEST_SKIP() << "No chips present on the system. Skipping test.";
+    }
+    std::unique_ptr<TTDevice> tt_device = TTDevice::create(pci_device_ids.at(0));
+
+    // In case of u6 galaxy and blackhole, we generate the cluster descriptor.
+    // For wormhole we still use create-ethernet-map.
+    std::filesystem::path cluster_path;
+    if (tt_device->get_arch() == tt::ARCH::BLACKHOLE || tt_device->get_board_type() == BoardType::UBB) {
+        cluster_path = tt::umd::Cluster::serialize_to_file();
+    } else {
+        cluster_path = tt_ClusterDescriptor::get_cluster_descriptor_file_path();
+    }
+
+    std::cout << "Cluster descriptor file path: " << cluster_path << std::endl;
+    std::cout << "Contents:" << std::endl;
+    std::ifstream file(cluster_path);  // open the file
+    std::string line;
+    while (std::getline(file, line)) {
+        std::cout << line << std::endl;
+    }
+    file.close();
 }

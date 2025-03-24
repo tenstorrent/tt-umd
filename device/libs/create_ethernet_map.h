409b0730f8033f375bda7c473370ba7b792c7e35
Andrew Fuller <afuller@tenstorrent.com>
2025-03-24 23:19:40 +0000

    remove last remnant -- requires non-prebuilt anyway

--- device/libs/create_ethernet_map.h
+++ device/libs/create_ethernet_map.h
@@ -6,5 +6,5 @@
 
 // External function to create the cluster descriptor yaml file.
 extern "C" {
-int create_ethernet_map(char *file) __attribute__((no_sanitize("thread")));
+int create_ethernet_map(char *file);
 }

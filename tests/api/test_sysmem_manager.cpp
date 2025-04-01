7769f8f88d6ce401787ed730fdc00beea7b4b272
Bojan Rosko <brosko@tenstorrent.com>
2025-04-01 12:08:44 +0000

    address test comments

--- tests/api/test_sysmem_manager.cpp
+++ tests/api/test_sysmem_manager.cpp
@@ -20,13 +20,22 @@ TEST(ApiSysmemManager, BasicIO) {
     // Initializes system memory with one channel.
     sysmem->init_hugepage(1);
 
+    // Simple write and read test.
     std::vector<uint32_t> data_write = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
     sysmem->write_to_sysmem(0, data_write.data(), 0, data_write.size() * sizeof(uint32_t));
 
+    std::vector<uint32_t> data_read = std::vector<uint32_t>(data_write.size(), 0);
+    sysmem->read_from_sysmem(0, data_read.data(), 0, data_read.size() * sizeof(uint32_t));
+
+    EXPECT_EQ(data_write, data_read);
+
+    // Channel 1 is not setup, so this should throw an exception.
     EXPECT_THROW(
         sysmem->write_to_sysmem(1, data_write.data(), 0, data_write.size() * sizeof(uint32_t)), std::runtime_error);
-    EXPECT_THROW(
-        sysmem->write_to_sysmem(0, data_write.data(), HUGEPAGE_REGION_SIZE + 1, data_write.size() * sizeof(uint32_t)),
-        std::runtime_error);
+
+    // When we write over the limit, the address is wrapped around the hugepage size
+    sysmem->write_to_sysmem(0, data_write.data(), HUGEPAGE_REGION_SIZE + 0x100, data_write.size() * sizeof(uint32_t));
+    data_read = std::vector<uint32_t>(data_write.size(), 0);
+    sysmem->read_from_sysmem(0, data_read.data(), 0x100, data_read.size() * sizeof(uint32_t));
+    EXPECT_EQ(data_write, data_read);
 }

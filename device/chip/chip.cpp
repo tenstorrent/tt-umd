9eb619852a1a4c74840fd4fdd3bb52f5926905a3
Bojan Roško <156314064+broskoTT@users.noreply.github.com>
2025-04-02 11:45:20 +0200

    Move read/write to device to Chip (#660)
    ### Issue
    Contributing towards #248
    
    ### Description
    Move local chip communication from Cluster to Chip
    
    ### List of the changes
    - Moved read/write device to LocalChip
    - Set some members in chip protected instead of private
    
    ### Testing
    Existing CI tests
    
    ### API Changes
    There are no API changes in this PR.

--- device/chip/chip.cpp
+++ device/chip/chip.cpp
@@ -76,6 +76,16 @@ void Chip::read_from_sysmem(uint16_t channel, void* dest, uint64_t sysmem_src, u
     throw std::runtime_error("Chip::read_from_sysmem is not available for this chip.");
 }
 
+void Chip::write_to_device(
+    tt_xy_pair core, const void* src, uint64_t l1_dest, uint32_t size, const std::string& fallback_tlb) {
+    throw std::runtime_error("Chip::write_to_device is not available for this chip.");
+}
+
+void Chip::read_from_device(
+    tt_xy_pair core, void* dest, uint64_t l1_src, uint32_t size, const std::string& fallback_tlb) {
+    throw std::runtime_error("Chip::read_from_device is not available for this chip.");
+}
+
 std::unique_lock<boost::interprocess::named_mutex> Chip::get_mutex(std::string mutex_name, int pci_device_id) {
     throw std::runtime_error("LockManager::get_mutex is not available for this chip.");
 }

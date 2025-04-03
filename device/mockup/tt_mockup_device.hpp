6d4eb0f44a98c5f8eead0f5c524bd85069a5c098
Bojan Rosko <brosko@tenstorrent.com>
2025-04-03 08:12:56 +0000

    fix tests and mockup

--- device/mockup/tt_mockup_device.hpp
+++ device/mockup/tt_mockup_device.hpp
@@ -23,6 +23,9 @@ public:
 
     void set_barrier_address_params(const barrier_address_params& barrier_address_params_) override {}
 
+    void configure_active_ethernet_cores_for_mmio_device(
+        chip_id_t mmio_chip, const std::unordered_set<tt::umd::CoreCoord>& active_eth_cores_per_chip) override {}
+
     void start_device(const tt_device_params& device_params) override {}
 
     void assert_risc_reset() override {}
@@ -30,23 +33,45 @@ public:
     void deassert_risc_reset() override {}
 
     void deassert_risc_reset_at_core(
-        tt_cxy_pair core, const TensixSoftResetOptions& soft_resets = TENSIX_DEASSERT_SOFT_RESET) override {}
+        const chip_id_t chip,
+        const tt::umd::CoreCoord core,
+        const TensixSoftResetOptions& soft_resets = TENSIX_DEASSERT_SOFT_RESET) override {}
 
     void assert_risc_reset_at_core(
-        tt_cxy_pair core, const TensixSoftResetOptions& soft_resets = TENSIX_ASSERT_SOFT_RESET) override {}
+        const chip_id_t chip,
+        const tt::umd::CoreCoord core,
+        const TensixSoftResetOptions& soft_resets = TENSIX_ASSERT_SOFT_RESET) override {}
 
     void close_device() override {}
 
-    // Runtime Functions
+    void wait_for_non_mmio_flush() override {}
+
+    void wait_for_non_mmio_flush(const chip_id_t chip_id) override {}
+
     void write_to_device(
         const void* mem_ptr,
         uint32_t size_in_bytes,
-        tt_cxy_pair core,
+        chip_id_t chip,
+        tt::umd::CoreCoord core,
         uint64_t addr,
         const std::string& tlb_to_use) override {}
 
+    void broadcast_write_to_cluster(
+        const void* mem_ptr,
+        uint32_t size_in_bytes,
+        uint64_t address,
+        const std::set<chip_id_t>& chips_to_exclude,
+        std::set<uint32_t>& rows_to_exclude,
+        std::set<uint32_t>& columns_to_exclude,
+        const std::string& fallback_tlb) override {}
+
     void read_from_device(
-        void* mem_ptr, tt_cxy_pair core, uint64_t addr, uint32_t size, const std::string& fallback_tlb) override {}
+        void* mem_ptr,
+        chip_id_t chip,
+        tt::umd::CoreCoord core,
+        uint64_t addr,
+        uint32_t size,
+        const std::string& fallback_tlb) override {}
 
     void write_to_sysmem(
         const void* mem_ptr, std::uint32_t size, uint64_t addr, uint16_t channel, chip_id_t src_device_id) override {}
@@ -57,7 +82,7 @@ public:
     void l1_membar(
         const chip_id_t chip,
         const std::string& fallback_tlb,
-        const std::unordered_set<tt_xy_pair>& cores = {}) override {}
+        const std::unordered_set<tt::umd::CoreCoord>& cores = {}) override {}
 
     void dram_membar(
         const chip_id_t chip,
@@ -67,12 +92,21 @@ public:
     void dram_membar(
         const chip_id_t chip,
         const std::string& fallback_tlb,
-        const std::unordered_set<tt_xy_pair>& cores = {}) override {}
+        const std::unordered_set<tt::umd::CoreCoord>& cores = {}) override {}
 
-    void wait_for_non_mmio_flush() override {}
+    int arc_msg(
+        int logical_device_id,
+        uint32_t msg_code,
+        bool wait_for_done = true,
+        uint32_t arg0 = 0,
+        uint32_t arg1 = 0,
+        int timeout = 1,
+        uint32_t* return_3 = nullptr,
+        uint32_t* return_4 = nullptr) override {
+        return 0;
+    }
 
-    // Misc. Functions to Query/Set Device State
-    static std::vector<chip_id_t> detect_available_device_ids() { return {0}; };
+    tt_ClusterDescriptor* get_cluster_description() override { return cluster_descriptor.get(); }
 
     std::set<chip_id_t> get_target_device_ids() override { return target_devices_in_cluster; }
 
@@ -82,11 +116,9 @@ public:
 
     std::map<int, int> get_clocks() override { return {{0, 0}}; }
 
-    void* host_dma_address(std::uint64_t offset, chip_id_t src_device_id, uint16_t channel) const override {
-        return nullptr;
-    }
+    std::uint32_t get_numa_node_for_pcie_device(std::uint32_t device_id) override { return 0; }
 
-    std::uint64_t get_pcie_base_addr_from_device(const chip_id_t chip_id) const override { return 0; }
+    tt_version get_ethernet_fw_version() const override { return {0, 0, 0}; }
 
     std::uint32_t get_num_dram_channels(std::uint32_t device_id) override {
         return get_soc_descriptor(device_id).get_num_dram_channels();
@@ -100,11 +132,18 @@ public:
 
     std::uint32_t get_host_channel_size(std::uint32_t device_id, std::uint32_t channel) override { return 0; }
 
-    std::uint32_t get_numa_node_for_pcie_device(std::uint32_t device_id) override { return 0; }
+    void* host_dma_address(std::uint64_t offset, chip_id_t src_device_id, uint16_t channel) const override {
+        return nullptr;
+    }
+
+    std::uint64_t get_pcie_base_addr_from_device(const chip_id_t chip_id) const override { return 0; }
 
     const tt_SocDescriptor& get_soc_descriptor(chip_id_t chip_id) const override {
         return soc_descriptor_per_chip.at(chip_id);
     };
+
+    // Misc. Functions to Query/Set Device State
+    static std::vector<chip_id_t> detect_available_device_ids() { return {0}; }
 
 private:
     std::vector<tt::ARCH> archs_in_cluster = {};

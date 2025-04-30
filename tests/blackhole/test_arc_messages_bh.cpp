f5e24214b55732028977fb1a0f9a15ad84f0d1c0
Bojan Roško <156314064+broskoTT@users.noreply.github.com>
2025-04-30 16:10:01 +0200

    Disable AICLK tests for busy value (#816)
    ### Issue
    Related to #781
    
    ### Description
    Until we figure out a proper mechanism, we shouldn't fail if we don't
    see the exact hardcoded busy value for aiclk.
    
    ### List of the changes
    - Changed all tests that expected BUSY_VAL to instead expect larget than
    IDLE_VAL
    
    ### Testing
    CI tests
    
    ### API Changes
    There are no API changes in this PR.

--- tests/blackhole/test_arc_messages_bh.cpp
+++ tests/blackhole/test_arc_messages_bh.cpp
@@ -45,7 +45,8 @@ TEST(BlackholeArcMessages, BlackholeArcMessageHigherAIClock) {
 
         uint32_t aiclk = tt_device->get_clock();
 
-        EXPECT_EQ(aiclk, blackhole::AICLK_BUSY_VAL);
+        // TODO #781: For now expect only that busy val is something larger than idle val.
+        EXPECT_GT(aiclk, blackhole::AICLK_IDLE_VAL);
 
         response = bh_arc_messenger->send_message((uint32_t)blackhole::ArcMessageType::AICLK_GO_LONG_IDLE);
 

aba2613b937d6a7a6474c78f93df6e7fb8427cb5
Pavle Janevski <165378935+pjanevskiTT@users.noreply.github.com>
2025-05-15 13:28:33 +0200

    Add ubench for PCIE DMA read/write (#844)
    ### Issue
    
    Part of the work coming from #751 
    
    ### Description
    
    Add ubench tests for PCIE DMA. These were written for the report of the
    perf for the above issue. Today, UMD doesn't have any smart logic to
    conclude what is the biggest DMA buffer that you can map, so it's using
    1MB buffer (IOMMU will solve this problem). It's important to conclude
    that DMA path is much better than TLB path in #837 (15 GB/s both
    read/write for DMA). We can expand on these tests when we have IOMMU
    fully enabled and integrated in order to test avoiding memcpy to DMA
    buffer.
    
    ### List of the changes
    
    - Add test for read/write over DMA to tensix 
    - Add test for read/write over DMA to DRAM
    - Sweep over different amounts of data to transfer
    
    ### Testing
    (Comment on CI testing or Manual testing touching this change.)
    
    ### API Changes
    /

--- tests/microbenchmark/test_perf_dma.cpp
+++ tests/microbenchmark/test_perf_dma.cpp
@@ -19,9 +19,7 @@ using namespace tt::umd;
 
 const chip_id_t chip = 0;
 const uint32_t one_mb = 1 << 20;
-const uint32_t NUM_ITERATIONS = 10;
-const uint32_t tlb_1m_index = 0;
-const uint32_t tlb_16m_index = 166;
+const uint32_t NUM_ITERATIONS = 1000;
 
 static inline void print_speed(std::string direction, size_t bytes, uint64_t ns) {
     double seconds = ns / 1e9;
@@ -48,7 +46,7 @@ static inline void perf_read_write(
 
     auto now = std::chrono::steady_clock::now();
     for (int i = 0; i < num_iterations; i++) {
-        cluster->write_to_device(pattern.data(), pattern.size(), chip, core, 0x0);
+        cluster->dma_write_to_device(pattern.data(), pattern.size(), chip, core, 0x0);
     }
     auto end = std::chrono::steady_clock::now();
     auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end - now).count();
@@ -57,88 +55,47 @@ static inline void perf_read_write(
     std::vector<uint8_t> readback(buf_size, 0x0);
     now = std::chrono::steady_clock::now();
     for (int i = 0; i < num_iterations; i++) {
-        cluster->read_from_device(readback.data(), chip, core, 0x0, readback.size());
+        cluster->dma_read_from_device(readback.data(), readback.size(), chip, core, 0x0);
     }
     end = std::chrono::steady_clock::now();
     ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end - now).count();
     print_speed(direction_from_device, num_iterations * readback.size(), ns);
 }
 
-TEST(TestPerf, TLBDynamicDram) {
+/**
+ * Test the PCIe DMA controller by using it to write random fixed-size pattern
+ * to 0x0 tensix core, then reading them back and verifying.
+ */
+TEST(TestPerf, DMATensix) {
+    const std::vector<uint32_t> sizes = {
+        1 * one_mb,
+    };
+
+    std::unique_ptr<Cluster> cluster = std::make_unique<Cluster>();
+    const CoreCoord tensix_core = cluster->get_soc_descriptor(chip).get_cores(CoreType::TENSIX)[0];
+    cluster->start_device(tt_device_params{});
+
+    for (uint32_t buf_size : sizes) {
+        perf_read_write(
+            buf_size,
+            NUM_ITERATIONS,
+            cluster,
+            tensix_core,
+            "DMA: Host -> Device Tensix L1",
+            "DMA: Device Tensix L1 -> Host");
+    }
+}
+
+/**
+ * Test the PCIe DMA controller by using it to write random fixed-size pattern
+ * to 0x0 DRAM core, then reading them back and verifying.
+ */
+TEST(TestPerf, DMADram) {
     const std::vector<uint32_t> sizes = {
         1 * one_mb,
         2 * one_mb,
         4 * one_mb,
         8 * one_mb,
-        16 * one_mb,
-        32 * one_mb,
-        64 * one_mb,
-        128 * one_mb,
-        256 * one_mb,
-    };
-
-    std::unique_ptr<Cluster> cluster = std::make_unique<Cluster>();
-    const CoreCoord dram_core = cluster->get_soc_descriptor(chip).get_cores(CoreType::DRAM)[0];
-    cluster->start_device(tt_device_params{});
-
-    for (uint32_t buf_size : sizes) {
-        perf_read_write(
-            buf_size,
-            NUM_ITERATIONS,
-            cluster,
-            dram_core,
-            "Dynamic TLB: Host -> Device DRAM",
-            "Dynamic TLB: Device DRAM -> Host");
-    }
-}
-
-TEST(TestPerf, TLBDynamicTensix) {
-    const std::vector<uint32_t> sizes = {
-        1 * one_mb,
-    };
-
-    std::unique_ptr<Cluster> cluster = std::make_unique<Cluster>();
-    const CoreCoord tensix_core = cluster->get_soc_descriptor(chip).get_cores(CoreType::TENSIX)[0];
-    cluster->start_device(tt_device_params{});
-
-    for (uint32_t buf_size : sizes) {
-        perf_read_write(
-            buf_size,
-            NUM_ITERATIONS,
-            cluster,
-            tensix_core,
-            "Dynamic TLB: Host -> Device Tensix L1",
-            "Dynamic TLB: Device Tensix L1 -> Host");
-    }
-}
-
-TEST(TestPerf, TLBStaticTensix) {
-    const size_t tlb_1m_index = 0;
-
-    std::unique_ptr<Cluster> cluster = std::make_unique<Cluster>();
-    const CoreCoord tensix_core = cluster->get_soc_descriptor(chip).get_cores(CoreType::TENSIX)[0];
-    cluster->start_device(tt_device_params{});
-
-    cluster->configure_tlb(0, tensix_core, tlb_1m_index, 0x0, TLB_DATA::Relaxed);
-
-    const std::vector<uint32_t> sizes = {
-        1 * one_mb,
-    };
-
-    for (uint32_t buf_size : sizes) {
-        const uint32_t num_io = buf_size / one_mb;
-        perf_read_write(
-            buf_size,
-            num_io,
-            cluster,
-            tensix_core,
-            "Static TLB: Host -> Device Tensix L1",
-            "Static TLB: Device Tensix L1 -> Host");
-    }
-}
-
-TEST(TestPerf, TLBStaticDram) {
-    const std::vector<uint32_t> sizes = {
         16 * one_mb,
         32 * one_mb,
         64 * one_mb,
@@ -151,13 +108,8 @@ TEST(TestPerf, TLBStaticDram) {
     std::unique_ptr<Cluster> cluster = std::make_unique<Cluster>();
     const CoreCoord dram_core = cluster->get_soc_descriptor(chip).get_cores(CoreType::DRAM)[0];
     cluster->start_device(tt_device_params{});
-
-    cluster->configure_tlb(0, dram_core, tlb_16m_index, 0x0, TLB_DATA::Relaxed);
-
     for (uint32_t buf_size : sizes) {
-        const uint32_t num_io = buf_size / (16 * one_mb);
-
         perf_read_write(
-            buf_size, num_io, cluster, dram_core, "Static TLB: Host -> Device DRAM", "Static TLB: Device DRAM -> Host");
+            buf_size, NUM_ITERATIONS, cluster, dram_core, "DMA: Host -> Device DRAM", "DMA: Device DRAM -> Host");
     }
 }

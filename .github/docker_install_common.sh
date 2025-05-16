da713690250880787efaaec4069d787e2c363754
blozano-tt <blozano@tenstorrent.com>
2025-05-16 01:38:21 +0000

    Revert "Try upgrading CMake in docker images"
    This reverts commit 4852a8b789cd33c2a2a90b409f81192dc22a381a.

--- .github/docker_install_common.sh
+++ .github/docker_install_common.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-UBUNTU_CODENAME=$(grep '^VERSION_CODENAME=' /etc/os-release | awk -F= '{print $2}' | tr -d '"')
-export UBUNTU_CODENAME
-
-# The below is to bring cmake from kitware
-apt-get update
-apt-get install -y --no-install-recommends ca-certificates gpg lsb-release wget software-properties-common gnupg jq
-wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
-echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ $UBUNTU_CODENAME main" | tee /etc/apt/sources.list.d/kitware.list >/dev/null
-
 # Install build and runtime deps
-apt-get update
-apt-get install -y \
+apt-get update && apt-get install -y \
     software-properties-common \
     build-essential \
     cmake \
@@ -44,5 +34,3 @@ wget https://apt.llvm.org/llvm.sh && \
 # Install clang-format
 apt install -y clang-format-17 && \
     ln -s /usr/bin/clang-format-17 /usr/bin/clang-format
-
-apt-get clean && rm -rf /var/lib/apt/lists/*

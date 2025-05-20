c52fca73d32cf2a8058cdb7907f56c66fea3cee9
blozano-tt <blozano@tenstorrent.com>
2025-05-20 18:04:20 +0000

    Add source location logging

--- device/logger_init/logger_init.cpp
+++ device/logger_init/logger_init.cpp
@@ -31,7 +31,7 @@ constexpr auto log_pattern = "[%Y-%m-%d %H:%M:%S.%e] [%l] [%s:%#] %v";
  * setting up either file-based or console-based logging depending on
  * the environment variable configuration.
  */
-static tt::LoggerInitializer loggerInitializer(file_env_var, level_env_var);
+static tt::LoggerInitializer loggerInitializer(file_env_var, level_env_var, log_pattern);
 
 // Note - Using default logger pattern, until source_location info can be fixed
 

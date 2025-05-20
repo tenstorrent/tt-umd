ea3fe974580eac5343c5fd35d0605c2ca62e82e2
Pavle Janevski <pjanevski@tenstorrent.com>
2025-05-20 12:17:50 +0000

    Create tlb handle without configuring it

--- device/tt_device/tlb_allocator.cpp
+++ device/tt_device/tlb_allocator.cpp
@@ -11,9 +11,7 @@ namespace tt::umd {
 TlbAllocator::TlbAllocator(PCIDevice* pci_device) : pci_device(pci_device) {}
 
 std::unique_ptr<TlbWindow> TlbAllocator::get_tlb(const uint64_t size, const tenstorrent_noc_tlb_config config) {
-    auto tlb_window = std::make_unique<TlbWindow>(std::make_unique<TlbHandle>(pci_device->get_fd(), size, config));
-    tlb_window->configure(config);
-    return tlb_window;
+    return std::make_unique<TlbWindow>(std::make_unique<TlbHandle>(pci_device->get_fd(), size), config);
 }
 
 }  // namespace tt::umd

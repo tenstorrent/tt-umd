0351d642304dc2da36705685ed4251ccf7848a6b
Pavle Janevski <pjanevski@tenstorrent.com>
2025-05-20 12:12:51 +0000

    Call configure from TLB window

--- device/tt_device/tlb_allocator.cpp
+++ device/tt_device/tlb_allocator.cpp
@@ -11,7 +11,9 @@ namespace tt::umd {
 TlbAllocator::TlbAllocator(PCIDevice* pci_device) : pci_device(pci_device) {}
 
 std::unique_ptr<TlbWindow> TlbAllocator::get_tlb(const uint64_t size, const tenstorrent_noc_tlb_config config) {
-    return std::make_unique<TlbWindow>(std::make_unique<TlbHandle>(pci_device->get_fd(), size, config));
+    auto tlb_window = std::make_unique<TlbWindow>(std::make_unique<TlbHandle>(pci_device->get_fd(), size, config));
+    tlb_window->configure(config);
+    return tlb_window;
 }
 
 }  // namespace tt::umd

57c24cd466e5dd652184532c374c6b1768d720fe
Bojan Roško <156314064+broskoTT@users.noreply.github.com>
2025-05-29 17:25:07 +0200

    Fix simulator build (#935)
    ### Issue
    I broke the simulator recently most probably in
    https://github.com/tenstorrent/tt-metal/pull/22078
    
    ### Description
    mock cluster was not being created anymore, leading to issues in
    tt_metal runtime.
    
    ### List of the changes
    - Create mock cluster if running on a an empty machine, and requesting
    non silicon chips
    - Fix fields filled in mock cluster descriptor
    
    ### Testing
    Manually tested that simulator tests pass with this change.
    
    ### API Changes
    There are no API changes in this PR.

--- device/cluster.cpp
+++ device/cluster.cpp
@@ -441,6 +441,25 @@ Cluster::Cluster(ClusterOptions options) {
     // For MOCK and SIMULATION chip types, passing target_devices which are not in cluster descriptor is allowed.
     if (options.chip_type == ChipType::SILICON) {
         chips_to_construct_vec = cluster_desc->get_chips_local_first(chips_to_construct);
+    } else {
+        // If we're running on a system where system chips don't match what was requested through target_chips for non
+        // silicon chip, then just create a mock cluster descriptor so we still have info for those chips. This includes
+        // systems with no silicon chips.
+        bool construct_mock_cluster_descriptor = false;
+        for (auto const& chip_id : chips_to_construct_vec) {
+            if (temp_full_cluster_desc->get_all_chips().find(chip_id) ==
+                temp_full_cluster_desc->get_all_chips().end()) {
+                log_warning(
+                    LogSiliconDriver,
+                    "Chip {} not found in cluster descriptor, creating mock cluster descriptor.",
+                    chip_id);
+                construct_mock_cluster_descriptor = true;
+                break;
+            }
+        }
+        if (construct_mock_cluster_descriptor) {
+            cluster_desc = tt_ClusterDescriptor::create_mock_cluster(chips_to_construct_vec, tt::ARCH::WORMHOLE_B0);
+        }
     }
     for (auto& chip_id : chips_to_construct_vec) {
         // Combine passed simulated_harvesting_masks

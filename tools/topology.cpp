4be2cd2d53a82507fefa65017afe3dea3ed0ad4b
Bojan Rosko <brosko@tenstorrent.com>
2025-06-03 13:03:51 +0000

    constrained topo

--- tools/topology.cpp
+++ tools/topology.cpp
@@ -4,6 +4,7 @@
 #include <cxxopts.hpp>
 #include <tt-logger/tt-logger.hpp>
 
+#include "common.h"
 #include "umd/device/cluster.h"
 #include "umd/device/tt_cluster_descriptor.h"
 
@@ -12,8 +13,13 @@ using namespace tt::umd;
 int main(int argc, char *argv[]) {
     cxxopts::Options options("topology", "Extract system topology and save it to a yaml file.");
 
-    options.add_options()("p,path", "File path to save cluster descriptor to.", cxxopts::value<std::string>())(
-        "h,help", "Print usage");
+    options.add_options()("f,path", "File path to save cluster descriptor to.", cxxopts::value<std::string>())(
+        "d,devices",
+        "List of logical device ids to filter cluster descriptor for.",
+        cxxopts::value<std::vector<std::string>>())(
+        "p,pci_devices",
+        "List of pci device ids to perform topology discovery on.",
+        cxxopts::value<std::vector<std::string>>())("h,help", "Print usage");
 
     auto result = options.parse(argc, argv);
 
@@ -27,7 +33,21 @@ int main(int argc, char *argv[]) {
         cluster_descriptor_path = result["path"].as<std::string>();
     }
 
-    std::string output_path = tt::umd::Cluster::create_cluster_descriptor()->serialize_to_file(cluster_descriptor_path);
+    std::unordered_set<int> pci_ids = {};
+    if (result.count("pci_devices")) {
+        pci_ids = extract_int_set(result["pci_devices"]);
+    }
+
+    std::unique_ptr<tt_ClusterDescriptor> cluster_descriptor = tt::umd::Cluster::create_cluster_descriptor("", pci_ids);
+
+    if (result.count("devices")) {
+        std::unordered_set<int> logical_device_ids = extract_int_set(result["devices"]);
+
+        cluster_descriptor =
+            tt_ClusterDescriptor::create_constrained_cluster_descriptor(cluster_descriptor.get(), logical_device_ids);
+    }
+
+    std::string output_path = cluster_descriptor->serialize_to_file(cluster_descriptor_path);
     log_info(tt::LogSiliconDriver, "Cluster descriptor serialized to {}", output_path);
     return 0;
 }

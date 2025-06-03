9d21152d69c68294920479ab94c28bac2620da5d
pjanevski <pjanevski@tenstorrent.com>
2025-06-03 12:12:28 +0000

    Add changes for KMD 2.0

--- device/ioctl.h
+++ device/ioctl.h
@@ -13,7 +13,7 @@
 #include <linux/types.h>
 #include <linux/ioctl.h>
 
-#define TENSTORRENT_DRIVER_VERSION 1
+#define TENSTORRENT_DRIVER_VERSION 2
 
 #define TENSTORRENT_IOCTL_MAGIC 0xFA
 
@@ -25,6 +25,8 @@
 #define TENSTORRENT_IOCTL_GET_DRIVER_INFO	_IO(TENSTORRENT_IOCTL_MAGIC, 5)
 #define TENSTORRENT_IOCTL_RESET_DEVICE		_IO(TENSTORRENT_IOCTL_MAGIC, 6)
 #define TENSTORRENT_IOCTL_PIN_PAGES		_IO(TENSTORRENT_IOCTL_MAGIC, 7)
+#define TENSTORRENT_IOCTL_LOCK_CTL		_IO(TENSTORRENT_IOCTL_MAGIC, 8)
+#define TENSTORRENT_IOCTL_MAP_PEER_BAR		_IO(TENSTORRENT_IOCTL_MAGIC, 9)
 #define TENSTORRENT_IOCTL_UNPIN_PAGES		_IO(TENSTORRENT_IOCTL_MAGIC, 10)
 #define TENSTORRENT_IOCTL_ALLOCATE_TLB		_IO(TENSTORRENT_IOCTL_MAGIC, 11)
 #define TENSTORRENT_IOCTL_FREE_TLB		_IO(TENSTORRENT_IOCTL_MAGIC, 12)
@@ -39,7 +41,10 @@
 #define TENSTORRENT_MAPPING_RESOURCE2_UC	5
 #define TENSTORRENT_MAPPING_RESOURCE2_WC	6
 
-#define TENSTORRENT_MAX_DMA_BUFS	8
+#define TENSTORRENT_MAX_DMA_BUFS	256
+#define TENSTORRENT_MAX_INBOUND_TLBS	256
+
+#define TENSTORRENT_RESOURCE_LOCK_COUNT 64
 
 struct tenstorrent_get_device_info_in {
 	__u32 output_size_bytes;
@@ -52,8 +57,8 @@ struct tenstorrent_get_device_info_out {
 	__u16 subsystem_vendor_id;
 	__u16 subsystem_id;
 	__u16 bus_dev_fn;	// [0:2] function, [3:7] device, [8:15] bus
-	__u16 max_dma_buf_size_log2;
-	__u16 pci_domain;
+	__u16 max_dma_buf_size_log2;	// Since 1.0
+	__u16 pci_domain;		// Since 1.23
 };
 
 struct tenstorrent_get_device_info {
@@ -127,6 +132,11 @@ struct tenstorrent_get_driver_info {
 	struct tenstorrent_get_driver_info_out out;
 };
 
+// tenstorrent_reset_device_in.flags
+#define TENSTORRENT_RESET_DEVICE_RESTORE_STATE 0
+#define TENSTORRENT_RESET_DEVICE_RESET_PCIE_LINK 1
+#define TENSTORRENT_RESET_DEVICE_CONFIG_WRITE 2
+
 struct tenstorrent_reset_device_in {
 	__u32 output_size_bytes;
 	__u32 flags;
@@ -143,7 +153,9 @@ struct tenstorrent_reset_device {
 };
 
 // tenstorrent_pin_pages_in.flags
-#define TENSTORRENT_PIN_PAGES_CONTIGUOUS 1
+#define TENSTORRENT_PIN_PAGES_CONTIGUOUS 1	// app attests that the pages are physically contiguous
+#define TENSTORRENT_PIN_PAGES_NOC_DMA 2		// app wants to use the pages for NOC DMA
+#define TENSTORRENT_PIN_PAGES_NOC_TOP_DOWN 4	// NOC DMA will be allocated top-down (default is bottom-up)
 
 struct tenstorrent_pin_pages_in {
 	__u32 output_size_bytes;
@@ -153,7 +165,7 @@ struct tenstorrent_pin_pages_in {
 };
 
 struct tenstorrent_pin_pages_out {
-	__u64 physical_address;
+	__u64 physical_address;	// or IOVA
 };
 
 struct tenstorrent_pin_pages {
@@ -161,9 +173,15 @@ struct tenstorrent_pin_pages {
 	struct tenstorrent_pin_pages_out out;
 };
 
+
+struct tenstorrent_pin_pages_out_extended {
+	__u64 physical_address;	// or IOVA
+	__u64 noc_address;
+};
+
+// unpinning subset of a pinned buffer is not supported
 struct tenstorrent_unpin_pages_in {
-	// Original VA used to pin, not current VA if remapped.
-	__u64 virtual_address;	
+	__u64 virtual_address;	// original VA used to pin, not current VA if remapped
 	__u64 size;
 	__u64 reserved;
 };
@@ -174,6 +192,44 @@ struct tenstorrent_unpin_pages_out {
 struct tenstorrent_unpin_pages {
 	struct tenstorrent_unpin_pages_in in;
 	struct tenstorrent_unpin_pages_out out;
+};
+
+// tenstorrent_lock_ctl_in.flags
+#define TENSTORRENT_LOCK_CTL_ACQUIRE 0
+#define TENSTORRENT_LOCK_CTL_RELEASE 1
+#define TENSTORRENT_LOCK_CTL_TEST 2
+
+struct tenstorrent_lock_ctl_in {
+	__u32 output_size_bytes;
+	__u32 flags;
+	__u8  index;
+};
+
+struct tenstorrent_lock_ctl_out {
+	__u8 value;
+};
+
+struct tenstorrent_lock_ctl {
+	struct tenstorrent_lock_ctl_in in;
+	struct tenstorrent_lock_ctl_out out;
+};
+
+struct tenstorrent_map_peer_bar_in {
+	__u32 peer_fd;
+	__u32 peer_bar_index;
+	__u32 peer_bar_offset;
+	__u32 peer_bar_length;
+	__u32 flags;
+};
+
+struct tenstorrent_map_peer_bar_out {
+	__u64 dma_address;
+	__u64 reserved;
+};
+
+struct tenstorrent_map_peer_bar {
+	struct tenstorrent_map_peer_bar_in in;
+	struct tenstorrent_map_peer_bar_out out;
 };
 
 struct tenstorrent_allocate_tlb_in {
--- device/pcie/pci_device.cpp
+++ device/pcie/pci_device.cpp
@@ -183,12 +183,24 @@ PCIDevice::PCIDevice(int pci_device_number) :
         TT_THROW("Running with IOMMU support requires KMD version {} or newer", kmd_ver_for_iommu.to_string());
     }
 
+    tenstorrent_get_driver_info driver_info{};
+    driver_info.in.output_size_bytes = sizeof(driver_info.out);
+    if (ioctl(pci_device_file_desc, TENSTORRENT_IOCTL_GET_DRIVER_INFO, &driver_info) == -1) {
+        TT_THROW("TENSTORRENT_IOCTL_GET_DRIVER_INFO failed");
+    }
+
     log_info(
         LogSiliconDriver,
         "Opened PCI device {}; KMD version: {}, IOMMU: {}",
+        "Opened PCI device {}; KMD version: {}; API: {}; IOMMU: {}",
         pci_device_num,
         kmd_version.to_string(),
+        driver_info.out.driver_version,
         iommu_enabled ? "enabled" : "disabled");
+
+    if (driver_info.out.driver_version < 2) {
+        TT_THROW("UMD requires a KMD version that supports APIv2 or greater.  Please update to the latest tt-kmd");
+    }
 
     TT_ASSERT(arch != tt::ARCH::WORMHOLE_B0 || revision == 0x01, "Wormhole B0 must have revision 0x01");
 

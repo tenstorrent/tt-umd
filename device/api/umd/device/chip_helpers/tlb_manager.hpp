32c17c55fc2f6d7feece3cc0d2bfdf010150cb4d
pjanevski <pjanevski@tenstorrent.com>
2025-06-04 15:03:08 +0200

    Reimplement UMD BAR0 usage

--- device/api/umd/device/chip_helpers/tlb_manager.hpp
+++ device/api/umd/device/chip_helpers/tlb_manager.hpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: (c) 2024 Tenstorrent Inc.
+ * SPDX-FileCopyrightText: (c) 2025 Tenstorrent Inc.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -23,13 +23,8 @@ public:
     TLBManager(TTDevice* tt_device);
 
     // All tt_xy_pairs should be in TRANSLATED coords.
-    void configure_tlb(tt_xy_pair core, int32_t tlb_index, uint64_t address, uint64_t ordering);
+    void configure_tlb(tt_xy_pair core, size_t tlb_size, uint64_t address, uint64_t ordering);
     void configure_tlb_kmd(tt_xy_pair core, size_t tlb_size, uint64_t address, uint64_t ordering);
-
-    void set_dynamic_tlb_config(std::string fallback_tlb_name, int32_t tlb_index);
-    void set_dynamic_tlb_config_ordering(std::string fallback_tlb_name, uint64_t ordering);
-
-    bool address_in_tlb_space(uint64_t address, uint32_t size_in_bytes, int32_t tlb_index, uint64_t tlb_size);
     bool is_tlb_mapped(tt_xy_pair core);
     bool is_tlb_mapped(tt_xy_pair core, uint64_t address, uint32_t size_in_bytes);
 
@@ -39,9 +34,6 @@ public:
     // TODO: the following members will be moved to private once enough stuff is moved out of cluster.
     std::unordered_map<int32_t, uint64_t> tlb_config_map_;
     std::unordered_map<tt_xy_pair, std::int32_t> map_core_to_tlb_;
-
-    std::unordered_map<std::string, std::int32_t> dynamic_tlb_config_;
-    std::unordered_map<std::string, uint64_t> dynamic_tlb_ordering_modes_;
     std::unordered_map<int32_t, std::unique_ptr<TlbWindow>> tlb_windows_;
 
     TTDevice* get_tt_device() { return tt_device_; }

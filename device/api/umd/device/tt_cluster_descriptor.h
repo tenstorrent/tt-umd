ea087c310e56957f8a58f111b96034cda8c70251
Bojan Rosko <brosko@tenstorrent.com>
2025-07-15 10:37:51 +0000

    resolved comments

--- device/api/umd/device/tt_cluster_descriptor.h
+++ device/api/umd/device/tt_cluster_descriptor.h
@@ -68,6 +68,9 @@ public:
 
     /**
      * Creates a constrained cluster descriptor that only contains the chips specified in target_chip_ids.
+     * @param full_cluster_desc Pointer to the full cluster descriptor from which the constrained descriptor will be
+     * created.
+     * @param target_chip_ids Set of logical chip IDs for filtering.
      */
     static std::unique_ptr<tt_ClusterDescriptor> create_constrained_cluster_descriptor(
         const tt_ClusterDescriptor *full_cluster_desc, const std::unordered_set<chip_id_t> &target_chip_ids);
@@ -77,14 +80,14 @@ public:
     /**
      * Return whether a chip is connected through a PCIe link.
      *
-     * @param chip_id Chip id to check.
+     * @param chip_id Logical chip id to check.
      */
     bool is_chip_mmio_capable(const chip_id_t chip_id) const;
 
     /**
      * Opposite of is_chip_mmio_capable.
      *
-     * @param chip_id Chip id to check.
+     * @param chip_id Logical chip id to check.
      */
     bool is_chip_remote(const chip_id_t chip_id) const;
 
@@ -94,7 +97,7 @@ public:
     std::size_t get_number_of_chips() const;
 
     /**
-     * Returns a set of logical chip ids for all chips in the cluster descriptor.
+     * Returns a set of logical chip IDs for all chips in the cluster descriptor.
      */
     const std::unordered_set<chip_id_t> &get_all_chips() const;
 
@@ -106,11 +109,13 @@ public:
 
     /**
      * Returns the architecture of a specific chip.
+     * @param chip_id Logical chip ID to get the architecture for.
      */
     tt::ARCH get_arch(chip_id_t chip_id) const;
 
     /**
      * Returns the board type of a specific chip.
+     * @param chip_id Logical chip ID to get the board type for.
      */
     BoardType get_board_type(chip_id_t chip_id) const;
 
@@ -122,62 +127,67 @@ public:
 
     /**
      * Returns board ID for a chip.
+     * @param chip Logical chip ID to get the board ID for.
      */
     uint64_t get_board_id_for_chip(const chip_id_t chip) const;
 
     /**
-     * Returns the map of chip IDs and information on wether NOC translation table is enabled for that chip.
+     * Returns the map of logical chip IDs and information on whether NOC translation table is enabled for that chip.
      */
     const std::unordered_map<chip_id_t, bool> &get_noc_translation_table_en() const;
 
     /**
-     * Returns the map of chip IDs and their ETH locations as reported by the routing firmware.
+     * Returns the map of logical chip IDs and their ETH coordinates as reported by the routing firmware.
      */
     const std::unordered_map<chip_id_t, eth_coord_t> &get_chip_locations() const;
 
     /**
-     * Returns the map of chip IDs and their ETH locations as reported by the routing firmware.
+     * Returns the map of logical chip IDs and their ETH locations as reported by the routing firmware.
      */
     const std::unordered_map<chip_id_t, uint64_t> &get_chip_unique_ids() const;
 
     /**
-     * Returns the map of chip IDs and their PCIe ids as reported by the operating system.
+     * Returns the map of logical chip IDs and their PCIe ids as reported by the operating system.
      */
     const std::unordered_map<chip_id_t, chip_id_t> &get_chips_with_mmio() const;
 
     // TODO: Remove the concept of ChipUUID.
     /**
-     * Returns the chip IDs for a given ChipUIDs.
+     * Returns the logical chip IDs for a given ChipUIDs.
      */
     std::optional<chip_id_t> get_chip_id(const ChipUID &chip_uid) const;
 
     /**
-     * Returns the ChipUID for a given chip ID.
+     * Returns the ChipUID for a given chip.
+     * @param chip_id Logical chip ID to get the ChipUID for.
      */
     std::optional<ChipUID> get_chip_uid(chip_id_t chip_id) const;
 
     /**
      * Returns the harvesting masks for a given chip ID.
-     * @param chip_id Chip ID to get the harvesting masks for.
+     * @param chip_id Logical chip ID to get the harvesting masks for.
      */
     tt::umd::HarvestingMasks get_harvesting_masks(chip_id_t chip_id) const;
 
     /* Connection related functions. */
     /**
      * Returns the closest PCIe connected chip. If passed chip is a PCIe chip, it will return itself.
-     * @param chip Chip id to check, can be a PCIe or remote chip.
+     * @param chip Logical chip id to check, can be a PCIe or remote chip.
      */
     chip_id_t get_closest_mmio_capable_chip(const chip_id_t chip);
+
     /*
      * Returns the pairs of channels that are connected where the first entry in the pair corresponds to the argument
-     * ordering when calling the function An empty result implies that the two chips do not share any direct connection
+     * ordering when calling the function An empty result implies that the two chips do not share any direct connection.
+     * @param first Logical chip id of the first chip.
+     * @param second Logical chip id of the second chip.
      */
     std::vector<std::tuple<ethernet_channel_t, ethernet_channel_t>>
     get_directly_connected_ethernet_channels_between_chips(const chip_id_t &first, const chip_id_t &second) const;
 
     /**
      * Returns a map representing all ethernet connections within the cluster.
-     * The map returned maps each chip and its core to a pair representing the remote chip and its core.
+     * The map returned maps each chip and its core to a pair representing the remote chip's logical id and its core.
      * All connections are bidirectional (each connection is reported twice).
      */
     const std::
@@ -186,7 +196,7 @@ public:
 
     /**
      * Returns a map representing all ethernet connections going outside of the cluster.
-     * The map returned maps each chip and its core to a pair representing the remote chip unique id and its core.
+     * The map returned maps each chip and its core to a pair representing the remote chip's unique id and its core.
      * All connections are bidirectional (each connection is reported twice).
      *
      * Note that in previous function the logical chip id is returned, but here we return unique chip id so it can be
@@ -199,6 +209,8 @@ public:
 
     /**
      * Return the distance between two chips in terms of ethernet hops.
+     * @param chip_a Logical chip id of the first chip.
+     * @param chip_b Logical chip id of the second chip.
      */
     int get_ethernet_link_distance(chip_id_t chip_a, chip_id_t chip_b) const;
 
@@ -211,12 +223,14 @@ public:
 
     /**
      * Returns the set of active ethernet channels for a given chip.
+     * @param chip_id Logical chip ID to check for active ethernet channels.
      */
     std::set<uint32_t> get_active_eth_channels(chip_id_t chip_id);
 
     /**
      * Returns the set of idle ethernet channels for a given chip.
      * Idle channels are those that are not currently used by any active ethernet link.
+     * @param chip_id Logical chip ID to check for idle ethernet channels.
      */
     std::set<uint32_t> get_idle_eth_channels(chip_id_t chip_id);
 

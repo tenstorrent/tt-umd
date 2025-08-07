1748e5d74192dfa1f303bf38cac60641c8b1cd2d
Pavle Janevski <pjanevski@tenstorrent.com>
2025-08-07 17:18:21 +0000

    Address comments

--- tests/api/test_cluster.cpp
+++ tests/api/test_cluster.cpp
@@ -115,6 +115,15 @@ TEST(ApiClusterTest, OpenChipsByPciId) {
             .pci_target_devices = {},
         });
 
+        if (!target_pci_device_ids.empty()) {
+            // If target_pci_device_ids is empty, then full cluster will be created, so skip the check.
+            // Check that the cluster has the expected number of chips.
+            auto actual_pci_device_ids = cluster->get_target_mmio_device_ids();
+            EXPECT_EQ(actual_pci_device_ids.size(), target_pci_device_ids.size());
+            // Always expect logical id 0 to exist, that's the way filtering by pci ids work.
+            EXPECT_TRUE(actual_pci_device_ids.find(0) != actual_pci_device_ids.end());
+        }
+
         if (unsetenv(TT_VISIBLE_DEVICES_ENV.data()) != 0) {
             ASSERT_TRUE(false) << "Failed to unset environment variable.";
         }

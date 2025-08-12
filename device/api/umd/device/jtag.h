54ed1cd171448a9ef0c5ab08d2be7448c6ac0016
uvelimirovicTT <uvelimirovic@tenstorrent.com>
2025-08-12 12:59:48 +0000

    Fixed formatting errors

--- device/api/umd/device/jtag.h
+++ device/api/umd/device/jtag.h
@@ -10,7 +10,7 @@
 #include <vector>
 
 class Jtag {
-   public:
+public:
     explicit Jtag(const char* libName);
     ~Jtag();
 
@@ -20,10 +20,14 @@ class Jtag {
     uint32_t readmon_tdr(const char* client, uint32_t id, uint32_t reg_offset);
     void writemon_tdr(const char* client, uint32_t id, uint32_t reg_offset, uint32_t data);
     void write_tdr(const char* client, uint32_t reg_offset, uint32_t data);
-    void dbus_memdump(const char* client_name, const char* mem, const char* thread_id_name, const char* start_addr,
-                      const char* end_addr);
-    void dbus_sigdump(const char* client_name, uint32_t dbg_client_id, uint32_t dbg_signal_sel_start,
-                      uint32_t dbg_signal_sel_end);
+    void dbus_memdump(
+        const char* client_name,
+        const char* mem,
+        const char* thread_id_name,
+        const char* start_addr,
+        const char* end_addr);
+    void dbus_sigdump(
+        const char* client_name, uint32_t dbg_client_id, uint32_t dbg_signal_sel_start, uint32_t dbg_signal_sel_end);
     void write_axi(uint32_t reg_addr, uint32_t data);
     void write_noc_xy(uint32_t node_x_id, uint32_t node_y_id, uint64_t noc_addr, uint32_t noc_data);
     uint32_t read_axi(uint32_t reg_addr);
@@ -33,11 +37,10 @@ class Jtag {
     uint32_t read_id_raw();
     uint32_t read_id();
 
-   private:
+private:
     void* handle;
     std::unordered_map<std::string, void*> funcMap;
     std::mutex mtx;
 
     void* loadFunction(const char* name);
 };
-

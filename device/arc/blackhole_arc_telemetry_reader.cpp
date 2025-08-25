6fbe3cacf1c811ed5a97833688a52d52548b2543
pjanevski <pjanevski@tenstorrent.com>
2025-08-25 23:28:24 +0200

    Fix BH

--- device/arc/blackhole_arc_telemetry_reader.cpp
+++ device/arc/blackhole_arc_telemetry_reader.cpp
@@ -22,12 +22,10 @@ BlackholeArcTelemetryReader::BlackholeArcTelemetryReader(TTDevice* tt_device) : 
 
 void BlackholeArcTelemetryReader::get_telemetry_address() {
     uint32_t telemetry_table_addr_u32;
-    tt_device->read_from_device(
-        &telemetry_table_addr_u32, arc_core, tt::umd::blackhole::SCRATCH_RAM_13, sizeof(uint32_t));
+    tt_device->read_from_arc(&telemetry_table_addr_u32, tt::umd::blackhole::SCRATCH_RAM_13, sizeof(uint32_t));
     telemetry_table_addr = telemetry_table_addr_u32;
     uint32_t telemetry_values_addr_u32;
-    tt_device->read_from_device(
-        &telemetry_values_addr_u32, arc_core, tt::umd::blackhole::SCRATCH_RAM_12, sizeof(uint32_t));
+    tt_device->read_from_arc(&telemetry_values_addr_u32, tt::umd::blackhole::SCRATCH_RAM_12, sizeof(uint32_t));
     telemetry_values_addr = telemetry_values_addr_u32;
 }
 

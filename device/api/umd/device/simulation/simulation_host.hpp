09f985f657a7ce7daa16463e6dbe27c53e45610f
Bojan Rosko <brosko@tenstorrent.com>
2025-09-01 10:02:58 +0000

    Revert "Revert "Switching how simulator and host connect to each other (#1228)" (#1236)"
    This reverts commit d5196f5ee90f0380848a19e199a68e8dab883eb6.

--- device/api/umd/device/simulation/simulation_host.hpp
+++ device/api/umd/device/simulation/simulation_host.hpp
@@ -9,10 +9,8 @@
 
 #include "umd/device/tt_xy_pair.h"
 
-#define NNG_SOCKET_PREFIX "ipc:///tmp/"
-
 typedef struct nng_socket_s nng_socket;
-typedef struct nng_dialer_s nng_dialer;
+typedef struct nng_listener_s nng_listener;
 
 namespace tt::umd {
 
@@ -27,7 +25,7 @@ public:
 
 private:
     std::unique_ptr<nng_socket> host_socket;
-    std::unique_ptr<nng_dialer> host_dialer;
+    std::unique_ptr<nng_listener> host_listener;
 };
 
 }  // namespace tt::umd

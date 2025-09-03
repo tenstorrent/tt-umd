9c081b261631446537792c872f8d3e6d0e71aff5
Bojan Rosko <brosko@tenstorrent.com>
2025-09-03 09:06:09 +0000

    fix build

--- tests/test_utils/test_api_common.hpp
+++ tests/test_utils/test_api_common.hpp
@@ -85,9 +85,9 @@ private:
 
 class ClusterReadWriteL1Test : public ::testing::TestWithParam<ClusterOptions> {};
 
-// Helper function to detect if any device in cluster is Galaxy board type
-inline bool is_galaxy_configuration(const Cluster* cluster) {
+// Helper function to detect if any device in cluster is Galaxy board type.
+inline bool is_galaxy_configuration(Cluster* cluster) {
     return cluster != nullptr && cluster->get_target_remote_device_ids().size() > 0 &&
-           cluster->get_cluster_description()->get_board_type(cluster->get_target_remote_device_ids()[0]) ==
+           cluster->get_cluster_description()->get_board_type(*cluster->get_target_remote_device_ids().begin()) ==
                BoardType::GALAXY;
 }

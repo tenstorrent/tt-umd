e47a5ddadb2a323fe45ae783e60892351cbb5a6b
Aleksa Markovic <aleksamarkovic@tenstorrent.com>
2025-09-12 08:50:25 +0000

    Remove surplus chip in WriteToAllChipsDram

--- tests/galaxy/test_umd_concurrent_threads.cpp
+++ tests/galaxy/test_umd_concurrent_threads.cpp
@@ -116,7 +116,7 @@ TEST(GalaxyConcurrentThreads, WriteToAllChipsL1) {
 TEST(GalaxyConcurrentThreads, WriteToAllChipsDram) {
     // Galaxy Setup
     std::shared_ptr<ClusterDescriptor> cluster_desc = Cluster::create_cluster_descriptor();
-    std::set<chip_id_t> target_devices_th1 = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32};
+    std::set<chip_id_t> target_devices_th1 = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30};
     std::set<chip_id_t> target_devices_th2 = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31};
     std::unordered_set<chip_id_t> all_devices = {};
     std::set_union(

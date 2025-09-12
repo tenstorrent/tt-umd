6be6d4995c143754df286837eb3bc29eeaebdaff
Vuk Jovanovic <vjovanovic@tenstorrent.com>
2025-09-12 10:14:31 +0000

    Trying to fix tests

--- common/utils.hpp
+++ common/utils.hpp
@@ -20,10 +20,10 @@ static std::string get_abs_path(std::string path) {
     std::filesystem::path current_file_path = std::filesystem::path(__FILE__);
     std::filesystem::path umd_root;
     if (current_file_path.is_absolute()) {
-        umd_root = current_file_path.parent_path().parent_path().parent_path();
+        umd_root = current_file_path.parent_path().parent_path();
     } else {
         std::filesystem::path umd_root_relative =
-            std::filesystem::relative(std::filesystem::path(__FILE__).parent_path().parent_path().parent_path(), "../");
+            std::filesystem::relative(std::filesystem::path(__FILE__).parent_path().parent_path(), "../");
         umd_root = std::filesystem::canonical(umd_root_relative);
     }
     std::filesystem::path abs_path = umd_root / path;

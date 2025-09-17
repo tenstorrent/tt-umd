fc76b3c5b9482c7995239882476aebac33807251
Pavle Janevski <pjanevski@tenstorrent.com>
2025-09-17 14:11:40 +0000

    debug dma allocation

--- tests/api/test_cluster.cpp
+++ tests/api/test_cluster.cpp
@@ -935,3 +935,25 @@ INSTANTIATE_TEST_SUITE_P(
     }
 
 );
+
+#include <fcntl.h>      // for ::open
+#include <linux/pci.h>  // for PCI_SLOT, PCI_FUNC
+#include <sys/ioctl.h>  // for ioctl
+#include <sys/mman.h>   // for mmap, munmap
+#include <unistd.h>     // for ::close
+
+#include "pcie/ioctl.h"
+
+TEST(ApiClusterTest, DmaAllocation) {
+    tenstorrent_allocate_dma_buf dma_buf{};
+    dma_buf.in.requested_size = 16 * (1 << 20);
+    dma_buf.in.buf_index = 0;
+
+    int pci_device_file_desc = open("/dev/tenstorrent/0", O_RDWR | O_CLOEXEC);
+
+    if (ioctl(pci_device_file_desc, TENSTORRENT_IOCTL_ALLOCATE_DMA_BUF, &dma_buf)) {
+        std::cout << "error" << std::endl;
+    } else {
+        std::cout << "managed to allocate" << std::endl;
+    }
+}

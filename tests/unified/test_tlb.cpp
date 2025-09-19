c49dde332474c64777453b7c0aa9814cf9f14654
Pavle Janevski <pjanevski@tenstorrent.com>
2025-09-19 11:55:45 +0000

    Add test for Writer class

--- tests/unified/test_tlb.cpp
+++ tests/unified/test_tlb.cpp
@@ -326,3 +326,34 @@ TEST(TestTlb, TestTlbAccessOutofBounds) {
             << "Reading out of bounds from TLB window should throw an exception";
     }
 }
+
+TEST(TestTlb, TLBStaticTensix) {
+    std::unique_ptr<Cluster> cluster = std::make_unique<Cluster>();
+
+    const CoreCoord tensix_core_0 = cluster->get_soc_descriptor(0).get_cores(CoreType::TENSIX)[0];
+    std::vector<uint32_t> zero_out(1024, 0);
+    std::vector<uint32_t> readback_zeros(1024, 0xFFFFFFFF);
+    cluster->write_to_device(zero_out.data(), zero_out.size() * sizeof(uint32_t), 0, tensix_core_0, 0);
+    cluster->read_from_device(readback_zeros.data(), 0, tensix_core_0, 0, readback_zeros.size() * sizeof(uint32_t));
+
+    EXPECT_EQ(readback_zeros, zero_out);
+
+    for (const CoreCoord tensix_core :
+         cluster->get_soc_descriptor(0).get_cores(CoreType::TENSIX, CoordSystem::VIRTUAL)) {
+        cluster->configure_tlb(0, {tensix_core.x, tensix_core.y}, 1 << 20, 0, tlb_data::Strict);
+    }
+
+    Writer writer = cluster->get_static_tlb_writer(0, tensix_core_0);
+
+    const int num_writes = 1024;
+    for (int i = 0; i < num_writes; i++) {
+        writer.write(4 * i, i);
+    }
+
+    std::vector<uint32_t> readback(num_writes, 0);
+    cluster->read_from_device(readback.data(), 0, tensix_core_0, 0, readback.size() * sizeof(uint32_t));
+
+    for (int i = 0; i < num_writes; i++) {
+        EXPECT_EQ(readback[i], i);
+    }
+}

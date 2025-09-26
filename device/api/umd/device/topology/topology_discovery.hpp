1e6221e9cc61406c62e6610e5ab11a0110673a00
pjanevski <pjanevski@tenstorrent.com>
2025-09-26 15:25:57 +0200

    Address comments

--- device/api/umd/device/topology/topology_discovery.hpp
+++ device/api/umd/device/topology/topology_discovery.hpp
@@ -139,7 +139,7 @@ protected:
 
     std::unordered_map<uint64_t, std::set<uint32_t>> active_eth_channels_per_chip;
 
-    // Needed to know which chip to use for remote communication.
+    // It's required to know which chip should be used for remote communication.
     std::map<uint64_t, uint64_t> remote_asic_id_to_mmio_chip_id = {};
 
     const std::string sdesc_path;

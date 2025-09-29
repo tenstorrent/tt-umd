edca1547aa65a4a10a35ef1ae46b3f5affef4140
Nenad Buncic <nbuncic@tenstorrent.com>
2025-09-29 12:43:34 +0000

    Small fix for Jtag Protocol

--- device/tt_device/jtag_protocol.cpp
+++ device/tt_device/jtag_protocol.cpp
@@ -5,14 +5,16 @@
  */
 #include "umd/device/tt_device/jtag_protocol.hpp"
 
+extern bool umd_use_noc1;
+
 namespace tt::umd {
 
 void JtagProtocol::write_to_device(const void* mem_ptr, tt_xy_pair core, uint64_t addr, uint32_t size) {
-    jtag_device_->write(jlink_id_, mem_ptr, core.x, core.y, addr, size);
+    jtag_device_->write(jlink_id_, mem_ptr, core.x, core.y, addr, size, umd_use_noc1 ? 1 : 0);
 }
 
 void JtagProtocol::read_from_device(void* mem_ptr, tt_xy_pair core, uint64_t addr, uint32_t size) {
-    jtag_device_->read(jlink_id_, mem_ptr, core.x, core.y, addr, size);
+    jtag_device_->read(jlink_id_, mem_ptr, core.x, core.y, addr, size, umd_use_noc1 ? 1 : 0);
 }
 
 void JtagProtocol::wait_for_non_mmio_flush() {}

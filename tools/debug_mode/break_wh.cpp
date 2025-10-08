76be4ef257e9b6db949caed9587e4d1b0427e75c
Aleksa Markovic <aleksamarkovic@tenstorrent.com>
2025-10-08 14:47:56 +0000

    update test

--- tools/debug_mode/break_wh.cpp
+++ tools/debug_mode/break_wh.cpp
@@ -8,10 +8,11 @@
 
 #include "umd/device/arch/wormhole_implementation.hpp"
 #include "umd/device/cluster.hpp"
-
+#include "umd/device/utils/debug_mode.hpp"
 using namespace tt::umd;
 
 int main(int argc, char* argv[]) {
+    tt::umd::DebugMode::enable();
     std::unique_ptr<Cluster> cluster = std::make_unique<Cluster>();
     if (cluster->get_cluster_description()->get_number_of_chips() == 0) {
         return 0;
@@ -25,8 +26,10 @@ int main(int argc, char* argv[]) {
     }
 
     chip->assert_risc_reset(RiscType::ALL);
-    std::vector<uint32_t> ebreak_instr_vector(131, 0x00100073);
+    std::vector<uint32_t> ebreak_instr_vector(0x3FFFF - 1, 0x00100073);
     CoreCoord eth_1 = chip->get_soc_descriptor().get_coord_at({1, 0}, CoordSystem::NOC0);
     chip->write_to_device(eth_1, ebreak_instr_vector.data(), 0x0, ebreak_instr_vector.size());
     chip->deassert_risc_reset(RiscType::ALL, false);
+
+    std::unique_ptr<Cluster> cluster2 = std::make_unique<Cluster>();
 }

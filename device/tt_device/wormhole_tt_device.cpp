a7cd0457e9f3e93b9dfc56ea19b45b27e3dc7077
asaigal <asaigal@tenstorrent.com>
2025-10-13 17:43:31 -0400

    Modifications to support Ethernet Link Retraining

--- device/tt_device/wormhole_tt_device.cpp
+++ device/tt_device/wormhole_tt_device.cpp
@@ -429,10 +429,8 @@ uint32_t WormholeTTDevice::wait_eth_core_training(const tt_xy_pair eth_core, con
         }
     }
 
-    uint32_t port_status = read_port_status(eth_core);
     start = std::chrono::system_clock::now();
-    while (port_status == ETH_UNKNOWN) {
-        port_status = read_port_status(eth_core);
+    while (read_training_status(eth_core) == LINK_TRAIN_TRAINING) {
         auto end = std::chrono::system_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
         time_taken_port = duration.count();
@@ -449,19 +447,15 @@ uint32_t WormholeTTDevice::wait_eth_core_training(const tt_xy_pair eth_core, con
 
 uint64_t WormholeTTDevice::get_arc_noc_base_address() const { return wormhole::ARC_NOC_XBAR_ADDRESS_START; }
 
-uint32_t WormholeTTDevice::read_port_status(tt_xy_pair eth_core) {
-    uint32_t channel = std::distance(
-        wormhole::ETH_CORES_NOC0.begin(),
-        std::find(wormhole::ETH_CORES_NOC0.begin(), wormhole::ETH_CORES_NOC0.end(), eth_core));
-
-    uint32_t port_status;
+uint32_t WormholeTTDevice::read_training_status(tt_xy_pair eth_core) {
+    uint32_t training_status;
     read_from_device(
-        &port_status,
+        &training_status,
         umd_use_noc1 ? tt_xy_pair(wormhole::NOC0_X_TO_NOC1_X[eth_core.x], wormhole::NOC0_Y_TO_NOC1_Y[eth_core.y])
                      : eth_core,
-        eth_addresses.eth_conn_info + (channel * 4),
+        0x1104,
         sizeof(uint32_t));
-    return port_status;
+    return training_status;
 }
 
 WormholeTTDevice::EthAddresses WormholeTTDevice::get_eth_addresses(const uint32_t eth_fw_version) {

43b5941937c007b385025131de92c3a96831d206
Aleksa Markovic <aleksamarkovic@tenstorrent.com>
2025-10-14 14:41:15 +0000

    Use <> include brackets for external code

--- tests/microbenchmark/benchmarks/pcie_dma/test_pcie_dma.cpp
+++ tests/microbenchmark/benchmarks/pcie_dma/test_pcie_dma.cpp
@@ -3,10 +3,10 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+#include <gtest/gtest.h>
 #include <sys/mman.h>
 
 #include "common/microbenchmark_utils.hpp"
-#include "gtest/gtest.h"
 #include "tests/test_utils/device_test_utils.hpp"
 
 using namespace tt::umd;

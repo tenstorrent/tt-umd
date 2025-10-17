0ee9007e7225b63706aa1ce973ef82297b41e4d3
Nenad Buncic <nbuncicTT@tenstorrent.com>
2025-10-17 06:44:48 +0000

    Pre-commit and add log_info

--- device/firmware/firmware_utils.cpp
+++ device/firmware/firmware_utils.cpp
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 #include "umd/device/firmware/firmware_utils.hpp"
+
 #include <cstdint>
 #include <thread>
 
@@ -27,22 +28,22 @@ semver_t get_firmware_version_util(TTDevice* tt_device) {
         std::unique_ptr<SmBusArcTelemetryReader> smbus_telemetry_reader =
             std::make_unique<SmBusArcTelemetryReader>(tt_device);
 
-    // Polling DEVICE_ID ensures SMBus telemetry is ready and returning valid data.
-    // If a valid DEVICE_ID is not detected within 250ms, a warning is logged and the curent value of 
-    // FW_BUNDLE_VERSION is returned.
-    auto start = std::chrono::steady_clock::now();
-    auto timeout_duration = std::chrono::milliseconds(250);
-    while (std::chrono::steady_clock::now() - start < timeout_duration) {
-        auto device_id = smbus_telemetry_reader->read_entry(tt::umd::wormhole::TelemetryTag::DEVICE_ID);
-        auto fw_version = fw_version_from_telemetry(
-            smbus_telemetry_reader->read_entry(tt::umd::wormhole::TelemetryTag::FW_BUNDLE_VERSION));
-        if(device_id == DEVICE_ID) {
-            return fw_version;
+        // Polling DEVICE_ID ensures SMBus telemetry is ready and returning valid data.
+        // If a valid DEVICE_ID is not detected within 250ms, a warning is logged and the curent value of
+        // FW_BUNDLE_VERSION is returned.
+        auto start = std::chrono::steady_clock::now();
+        auto timeout_duration = std::chrono::milliseconds(250);
+        while (std::chrono::steady_clock::now() - start < timeout_duration) {
+            auto device_id = smbus_telemetry_reader->read_entry(tt::umd::wormhole::TelemetryTag::DEVICE_ID);
+            auto fw_version = fw_version_from_telemetry(
+                smbus_telemetry_reader->read_entry(tt::umd::wormhole::TelemetryTag::FW_BUNDLE_VERSION));
+            if (device_id == DEVICE_ID) {
+                return fw_version;
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    }
-    log_warning(tt::LogUMD, "Didn't find valid firmware bundle version after 250ms");
-    return fw_version_from_telemetry(
+        log_warning(tt::LogUMD, "Didn't find valid firmware bundle version after 250ms");
+        return fw_version_from_telemetry(
             smbus_telemetry_reader->read_entry(tt::umd::wormhole::TelemetryTag::FW_BUNDLE_VERSION));
     }
     ArcTelemetryReader* telemetry = tt_device->get_arc_telemetry_reader();

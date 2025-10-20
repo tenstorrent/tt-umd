ca7217786dd2406b6a5c7196f93588178e03bb8f
Reku Dyu <rekudyu@protonmail.com>
2025-10-20 07:02:14 +0530

    replaced tt_version with semver_t

--- device/api/umd/device/cluster.hpp
+++ device/api/umd/device/cluster.hpp
@@ -18,6 +18,7 @@
 #include <unordered_set>
 #include <vector>
 
+#include "umd/device/utils/semver.hpp"
 #include "umd/device/chip/chip.hpp"
 #include "umd/device/cluster_descriptor.hpp"
 #include "umd/device/tt_device/tt_device.hpp"
@@ -613,7 +614,7 @@ public:
      * Get the ethernet firmware version used by the physical cluster (only implemented for Silicon Backend).
      * Will return a bogus version if no remote chips are supported for the device.
      */
-    tt_version get_ethernet_fw_version() const;
+    tt::umd::semver_t get_ethernet_fw_version() const;
 
     //---------- Functions to get various internal cluster objects, mainly device classes and their components.
 
@@ -729,7 +730,7 @@ private:
     std::map<std::set<ChipId>, std::unordered_map<ChipId, std::vector<std::vector<int>>>> bcast_header_cache = {};
     bool use_ethernet_broadcast = true;
     bool use_translated_coords_for_eth_broadcast = true;
-    tt_version eth_fw_version;  // Ethernet FW the driver is interfacing with
+    tt::umd::semver_t eth_fw_version;  // Ethernet FW the driver is interfacing with
     // ERISC FW Version Required by UMD
     static constexpr std::uint32_t SW_VERSION = 0x06060000;
 };

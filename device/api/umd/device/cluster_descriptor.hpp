86bf520b9b0f6b55a735264eee15af38304c341b
Pavle Janevski <165378935+pjanevskiTT@users.noreply.github.com>
2025-10-21 16:47:24 +0200

    Add verification for harvesting info of Tenstorrent cards (#1487)
    ### Issue
    
    We have some cards in the fleet that don't follow the spec for number of
    available Tensix, DRAM or ETH cores. Even if UMD can work with all of
    those cards, upper layers of stack have some expectations on card
    configuration so they might see diff in perf or something else and be
    surprised.
    
    ### Description
    
    Check number of harvested units for Tensix, DRAM and ETH. Every card has
    the spec on TT website on number of harvested units expected. Check
    these units and log warning if something is not as expected. We don't
    want to block people to work on these cards but it would be nice to let
    user know the card is not matching official product spec.
    
    ### List of the changes
    
    - Split verify functions in cluster descriptor into multiple functions
    - Add function to check expected number of harvested tensix
    - Add function to check expecetd number of harvested DRAM
    - Add function to check expected number of harvested ETH
    - Use these functions to verify harvesting info in cluster descriptor
    
    ### Testing
    
    CI + local testing
    
    ### API Changes
    /

--- device/api/umd/device/cluster_descriptor.hpp
+++ device/api/umd/device/cluster_descriptor.hpp
@@ -259,10 +259,16 @@ private:
     void fill_chips_grouped_by_closest_mmio();
 
     // Verify for some common mistakes.
-    void verify_cluster_descriptor_info();
+    bool verify_cluster_descriptor_info();
 
     // Return the default randomly generated path for serializing cluster descriptors.
     std::filesystem::path get_default_cluster_descriptor_file_path() const;
+
+    bool verify_board_info_for_chips();
+
+    bool verify_same_architecture();
+
+    bool verify_harvesting_information();
 
     std::unordered_map<ChipId, std::unordered_map<EthernetChannel, std::tuple<ChipId, EthernetChannel>>>
         ethernet_connections;

c4b8eb116eccf1d40068bd41b356e44535aa9125
Bojan Rosko <brosko@tenstorrent.com>
2025-10-30 12:13:20 +0000

    remove semver

--- nanobind/py_api_basic_types.cpp
+++ nanobind/py_api_basic_types.cpp
@@ -48,24 +48,6 @@ void bind_basic_types(nb::module_ &m) {
         .def("__int__", [](tt::ARCH tag) { return static_cast<int>(tag); })
         .def_static("from_str", &tt::arch_from_str, nb::arg("arch_str"));
 
-    nb::class_<semver_t>(m, "semver_t")
-        .def(nb::init<uint64_t, uint64_t, uint64_t>(), nb::arg("major"), nb::arg("minor"), nb::arg("patch"))
-        .def(nb::init<const std::string &>(), nb::arg("version_str"))
-        .def_rw("major", &semver_t::major)
-        .def_rw("minor", &semver_t::minor)
-        .def_rw("patch", &semver_t::patch)
-        .def("__str__", &semver_t::to_string)
-        .def(
-            "__repr__",
-            [](const semver_t &v) { return fmt::format("semver_t({}, {}, {})", v.major, v.minor, v.patch); })
-        .def("__lt__", &semver_t::operator<)
-        .def("__gt__", &semver_t::operator>)
-        .def("__eq__", &semver_t::operator==)
-        .def("__ne__", &semver_t::operator!=)
-        .def("__le__", &semver_t::operator<=)
-        .def("__ge__", &semver_t::operator>=)
-        .def_static("compare_firmware_bundle", &semver_t::compare_firmware_bundle, nb::arg("v1"), nb::arg("v2"));
-
     nb::enum_<tt::BoardType>(m, "BoardType")
         .value("E75", tt::BoardType::E75)
         .value("E150", tt::BoardType::E150)

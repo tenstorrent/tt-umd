d47bc27b9d24b114034863c53a55e4df2590e4ed
Nenad Buncic <nbuncic@tenstorrent.com>
2025-10-31 17:51:00 +0000

    Fix nanobind 3

--- nanobind/py_api_tt_device.cpp
+++ nanobind/py_api_tt_device.cpp
@@ -3,6 +3,7 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+#include <nanobind/chrono.h>
 #include <nanobind/nanobind.h>
 #include <nanobind/stl/shared_ptr.h>
 #include <nanobind/stl/string.h>
@@ -70,7 +71,7 @@ void bind_tt_device(nb::module_ &m) {
             nb::arg("device_number"),
             nb::arg("device_type") = IODeviceType::PCIe,
             nb::rv_policy::take_ownership)
-        .def("init_tt_device", &TTDevice::init_tt_device, nb::arg("timeout_ms"))
+        .def("init_tt_device", &TTDevice::init_tt_device, nb::arg("timeout_ms") = 5000)
         .def("get_arc_telemetry_reader", &TTDevice::get_arc_telemetry_reader, nb::rv_policy::reference_internal)
         .def("get_arch", &TTDevice::get_arch)
         .def("get_board_id", &TTDevice::get_board_id)

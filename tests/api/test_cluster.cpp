d7d339dce1f5a209f83c60241e07f5407c88f6d4
Nenad Buncic <nbuncic@tenstorrent.com>
2025-11-03 10:50:55 +0000

    Test new reset

--- tests/api/test_cluster.cpp
+++ tests/api/test_cluster.cpp
@@ -505,7 +505,7 @@ TEST(TestCluster, TestClusterAICLKControl) {
     }
 }
 
-TEST(TestCluster, DISABLED_WarmResetScratch) {
+TEST(TestCluster, WarmResetScratch) {
     std::unique_ptr<Cluster> cluster = std::make_unique<Cluster>();
 
     if (cluster->get_target_device_ids().empty()) {

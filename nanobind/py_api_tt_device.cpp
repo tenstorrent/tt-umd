fc66fc01e1ac004a3849767cde9f4a3f961c76e6
Bojan Rosko <brosko@tenstorrent.com>
2025-11-03 14:09:32 +0000

    add pci_device.device_id

--- nanobind/py_api_tt_device.cpp
+++ nanobind/py_api_tt_device.cpp
@@ -42,6 +42,8 @@ void bind_tt_device(nb::module_ &m) {
     nb::enum_<IODeviceType>(m, "IODeviceType").value("PCIe", IODeviceType::PCIe).value("JTAG", IODeviceType::JTAG);
 
     nb::class_<PciDeviceInfo>(m, "PciDeviceInfo")
+        .def_ro("vendor_id", &PciDeviceInfo::vendor_id)
+        .def_ro("device_id", &PciDeviceInfo::device_id)
         .def_ro("pci_domain", &PciDeviceInfo::pci_domain)
         .def_ro("pci_bus", &PciDeviceInfo::pci_bus)
         .def_ro("pci_device", &PciDeviceInfo::pci_device)

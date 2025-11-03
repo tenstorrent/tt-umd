6d897150305f965f3647ac9f35dcef7bfabc868a
Nenad Buncic <nbuncic@tenstorrent.com>
2025-11-03 10:18:42 +0000

    Polish for PR

--- device/api/umd/device/utils/timeouts.hpp
+++ device/api/umd/device/utils/timeouts.hpp
@@ -22,6 +22,8 @@ inline constexpr auto ETH_TRAINING_TIMEOUT = std::chrono::milliseconds(60'000);
 
 inline constexpr auto AICLK_TIMEOUT = std::chrono::milliseconds(100);
 
+inline constexpr auto WARM_RESET_DEVICES_REAPPEAR_TIMEOUT = std::chrono::milliseconds(10'000);
+
 inline constexpr auto UBB_WARM_RESET_TIMEOUT = std::chrono::milliseconds(100'000);
 inline constexpr auto BH_WARM_RESET_TIMEOUT = std::chrono::milliseconds(2'000);
 }  // namespace tt::umd::timeout

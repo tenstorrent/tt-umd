631465fa6d4b4138add9d5635275db96fbd54d0f
Bojan Roško <156314064+broskoTT@users.noreply.github.com>
2025-11-07 21:17:42 +0100

    is_mapping_buffer_to_noc_supported check enabled (#1462)
    ### Issue
    #1229 
    https://github.com/tenstorrent/tt-umd/issues/1542
    
    ### Description
    Enabling mapping host memory buffers through KMD.
    This was disabled since previously if multiple runtimes of metal were
    running, each of it would get a different set of noc addresses which
    wouldn't behave well in the current situation.
    
    ### List of the changes
    - Enable mapping buffer to noc through KMD
    - Enhance prints
    - Fix the issue when unmapping the 4th hugepage channel
    
    ### Testing
    All runs on brosko/temp_test_mapping_buf_kmd :
    https://github.com/tenstorrent/tt-metal/actions/runs/18598308290
    t3k tests were failing, but finally got a passing one:
    
    https://github.com/tenstorrent/tt-metal/actions/runs/19178760175/job/54831441727
    
    ### API Changes
    There are no API changes in this PR.

--- device/pcie/pci_device.cpp
+++ device/pcie/pci_device.cpp
@@ -525,6 +525,13 @@ uint64_t PCIDevice::map_for_hugepage(void *buffer, size_t size) {
     pin_pages.in.size = size;
 
     if (ioctl(pci_device_file_desc, TENSTORRENT_IOCTL_PIN_PAGES, &pin_pages) == -1) {
+        log_warning(
+            LogUMD,
+            "Failed to pin pages for hugepage at virtual address {} with size {} and flags {}: {}",
+            fmt::format("{:#x}", pin_pages.in.virtual_address),
+            fmt::format("{:#x}", pin_pages.in.size),
+            fmt::format("{:#x}", pin_pages.in.flags),
+            strerror(errno));
         return 0;
     }
 
@@ -538,12 +545,7 @@ uint64_t PCIDevice::map_for_hugepage(void *buffer, size_t size) {
     return pin_pages.out.physical_address;
 }
 
-bool PCIDevice::is_mapping_buffer_to_noc_supported() {
-    // return PCIDevice::read_kmd_version() >= kmd_ver_for_map_to_noc;
-    // TODO: This feature is turned off for now. We'll enable it once all machines have smoothly transitioned to IOMMU.
-    // Also change other places in this function which have the same check.
-    return false;
-}
+bool PCIDevice::is_mapping_buffer_to_noc_supported() { return PCIDevice::read_kmd_version() >= kmd_ver_for_map_to_noc; }
 
 std::pair<uint64_t, uint64_t> PCIDevice::map_buffer_to_noc(void *buffer, size_t size) {
     if (PCIDevice::read_kmd_version() < kmd_ver_for_map_to_noc) {
@@ -572,7 +574,12 @@ std::pair<uint64_t, uint64_t> PCIDevice::map_buffer_to_noc(void *buffer, size_t 
     pin.in.size = size;
 
     if (ioctl(pci_device_file_desc, TENSTORRENT_IOCTL_PIN_PAGES, &pin) == -1) {
-        TT_THROW("Failed to pin pages for DMA: {}", strerror(errno));
+        TT_THROW(
+            "Failed to pin pages for DMA buffer at virtual address {} with size {} and flags {}: {}",
+            fmt::format("{:#x}", pin.in.virtual_address),
+            fmt::format("{:#x}", pin.in.size),
+            fmt::format("{:#x}", pin.in.flags),
+            strerror(errno));
     }
 
     log_info(
@@ -619,7 +626,12 @@ std::pair<uint64_t, uint64_t> PCIDevice::map_hugepage_to_noc(void *hugepage, siz
     pin.in.size = size;
 
     if (ioctl(pci_device_file_desc, TENSTORRENT_IOCTL_PIN_PAGES, &pin) == -1) {
-        TT_THROW("Failed to pin pages for DMA: {} {}", strerror(errno), pin.in.flags);
+        TT_THROW(
+            "Failed to pin pages for hugepage at virtual address {} with size {} and flags {}: {}",
+            fmt::format("{:#x}", pin.in.virtual_address),
+            fmt::format("{:#x}", pin.in.size),
+            fmt::format("{:#x}", pin.in.flags),
+            strerror(errno));
     }
 
     log_info(
@@ -651,7 +663,12 @@ uint64_t PCIDevice::map_for_dma(void *buffer, size_t size) {
     pin_pages.in.size = size;
 
     if (ioctl(pci_device_file_desc, TENSTORRENT_IOCTL_PIN_PAGES, &pin_pages) == -1) {
-        TT_THROW("Failed to pin pages for DMA: {}", strerror(errno));
+        TT_THROW(
+            "Failed to pin pages for DMA buffer at virtual address {} with size {} and flags {}: {}",
+            fmt::format("{:#x}", pin_pages.in.virtual_address),
+            fmt::format("{:#x}", pin_pages.in.size),
+            fmt::format("{:#x}", pin_pages.in.flags),
+            strerror(errno));
     }
 
     log_info(
@@ -679,7 +696,11 @@ void PCIDevice::unmap_for_dma(void *buffer, size_t size) {
     unpin_pages.in.size = size;
 
     if (ioctl(pci_device_file_desc, TENSTORRENT_IOCTL_UNPIN_PAGES, &unpin_pages) < 0) {
-        TT_THROW("Failed to unpin pages for DMA buffer: {}", strerror(errno));
+        TT_THROW(
+            "Failed to unpin pages for DMA buffer at virtual address {} and size {}: {}",
+            fmt::format("{:#x}", vaddr),
+            fmt::format("{:#x}", size),
+            strerror(errno));
     }
 
     log_info(

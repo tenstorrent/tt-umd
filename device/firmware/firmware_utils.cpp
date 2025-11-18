bd28ac24a51b7fbe7400db15d5a016924c732f56
Bojan Rosko <brosko@tenstorrent.com>
2025-11-18 16:12:27 +0000

    Revert "Set expected ETH FW version according to FW bundle version (#1601)"
    This reverts commit 633003c397887a8c12ac5e36e25d1d051fe5e842.

--- device/firmware/firmware_utils.cpp
+++ device/firmware/firmware_utils.cpp
@@ -6,16 +6,12 @@
 #include "umd/device/firmware/firmware_utils.hpp"
 
 #include <cstdint>
-#include <optional>
 #include <thread>
-#include <tt-logger/tt-logger.hpp>
 
+#include "tt-logger/tt-logger.hpp"
 #include "umd/device/arc/smbus_arc_telemetry_reader.hpp"
-#include "umd/device/firmware/erisc_firmware.hpp"
-#include "umd/device/types/arch.hpp"
 #include "umd/device/types/telemetry.hpp"
 #include "umd/device/types/wormhole_telemetry.hpp"
-#include "umd/device/utils/semver.hpp"
 
 namespace tt::umd {
 semver_t fw_version_from_telemetry(const uint32_t telemetry_data) {
@@ -52,39 +48,6 @@ semver_t get_firmware_version_util(TTDevice* tt_device) {
     return telemetry->is_entry_available(TelemetryTag::FLASH_BUNDLE_VERSION)
                ? fw_version_from_telemetry(telemetry->read_entry(TelemetryTag::FLASH_BUNDLE_VERSION))
                : semver_t(0, 0, 0);
-}
-
-std::optional<semver_t> get_expected_eth_firmware_version_from_firmware_bundle(
-    semver_t fw_bundle_version, tt::ARCH arch) {
-    const auto* version_map = &erisc_firmware::WH_ERISC_FW_VERSION_MAP;
-    switch (arch) {
-        case ARCH::WORMHOLE_B0:
-            version_map = &erisc_firmware::WH_ERISC_FW_VERSION_MAP;
-            break;
-        case ARCH::BLACKHOLE:
-            version_map = &erisc_firmware::BH_ERISC_FW_VERSION_MAP;
-            break;
-        default:
-            return std::nullopt;
-    }
-
-    // Find the most recently updated ERISC FW version from a given firmware
-    // bundle version.
-    auto it = std::upper_bound(
-        version_map->begin(),
-        version_map->end(),
-        fw_bundle_version,
-        [](const semver_t& version, const std::pair<semver_t, semver_t>& entry) {
-            // Use special comparison function that handles legacy FW bundle versions.
-            return semver_t::compare_firmware_bundle(version, entry.first);
-        });
-
-    if (it != version_map->begin()) {
-        --it;
-        return it->second;
-    }
-
-    return std::nullopt;
 }
 
 semver_t get_eth_fw_version_from_telemetry(const uint32_t telemetry_data, tt::ARCH arch) {

b583661d3a1f871d9522300c1c8bc20bdd5c501f
asaigal <asaigal@tenstorrent.com>
2026-01-07 01:36:40 +0000

    Remove check comparing live ERISC FW version against FW bundle

--- device/firmware/firmware_utils.cpp
+++ device/firmware/firmware_utils.cpp
@@ -28,7 +28,8 @@ semver_t fw_version_from_telemetry(const uint32_t telemetry_data) {
     // lower 16 bits are the minor value.
     uint16_t major = (telemetry_data >> 24) & 0xFF;
     uint16_t minor = (telemetry_data >> 16) & 0xFF;
-    return semver_t(major, minor, 0);
+    uint16_t patch = (telemetry_data >> 8) & 0xFF;
+    return semver_t(major, minor, patch);
 }
 
 semver_t get_firmware_version_util(TTDevice* tt_device) {

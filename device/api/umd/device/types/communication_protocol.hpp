058755c82ec0fc4c2b50b962734f6fe3d755c482
Aleksa Markovic <aleksamarkovic@tenstorrent.com>
2026-01-12 15:50:46 +0000

    Correctly initialize communication_device_type in TTDevice

--- device/api/umd/device/types/communication_protocol.hpp
+++ device/api/umd/device/types/communication_protocol.hpp
@@ -11,12 +11,14 @@ namespace tt::umd {
 enum class IODeviceType {
     PCIe,
     JTAG,
+    UNKNOWN,
 };
 
 // Const map of Device type names for each of the types listed in the enum.
 static const std::unordered_map<IODeviceType, std::string> DeviceTypeToString = {
     {IODeviceType::PCIe, "PCIe"},
     {IODeviceType::JTAG, "JTAG"},
+    {IODeviceType::UNKNOWN, "Unknown"},
 };
 
 }  // namespace tt::umd

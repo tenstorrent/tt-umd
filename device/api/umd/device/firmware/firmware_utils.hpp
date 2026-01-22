1e23110156b2f4761c0109e426d7742eb5bf6cdc
Aleksa Markovic <aleksamarkovic@tenstorrent.com>
2026-01-22 13:50:12 +0000

    fix

--- device/api/umd/device/firmware/firmware_utils.hpp
+++ device/api/umd/device/firmware/firmware_utils.hpp
@@ -12,8 +12,6 @@
 #include "umd/device/utils/semver.hpp"
 
 namespace tt::umd {
-static inline semver_t fw_version_from_telemetry(const uint32_t telemetry_data);
-
 semver_t get_firmware_version_util(TTDevice* tt_device);
 
 std::optional<semver_t> get_expected_eth_firmware_version_from_firmware_bundle(

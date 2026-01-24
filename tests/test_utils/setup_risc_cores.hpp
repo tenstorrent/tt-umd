9c20bcfb02bfc416ef36d4bd4ac7418801b0749a
Nenad Buncic <nbuncic@tenstorrent.com>
2026-01-24 16:54:14 +0000

    Add lock for critical part 2

--- tests/test_utils/setup_risc_cores.hpp
+++ tests/test_utils/setup_risc_cores.hpp
@@ -15,26 +15,26 @@ using namespace tt::umd;
 namespace test_utils {
 
 inline void safe_test_cluster_start(Cluster* cluster) {
-    auto architecture = cluster->get_chip(0)->get_tt_device()->get_arch();
-    std::array<uint32_t, 12> brisc_program_default{};
-    std::copy(
-        brisc_configuration_program_default.cbegin(),
-        brisc_configuration_program_default.cend(),
-        std::next(brisc_program_default.begin(), 1));
-
-    switch (architecture) {
-        case tt::ARCH::WORMHOLE_B0:
-            brisc_program_default[0] = WORMHOLE_BRISC_BASE_INSTRUCTION;
-            break;
-        case tt::ARCH::BLACKHOLE:
-            brisc_program_default[0] = BLACKHOLE_BRISC_BASE_INSTRUCTION;
-            break;
-        default:
-            return;
-    }
-
     static std::mutex mtx;
     {
+        auto architecture = cluster->get_chip(0)->get_tt_device()->get_arch();
+        std::array<uint32_t, 12> brisc_program_default{};
+        std::copy(
+            brisc_configuration_program_default.cbegin(),
+            brisc_configuration_program_default.cend(),
+            std::next(brisc_program_default.begin(), 1));
+
+        switch (architecture) {
+            case tt::ARCH::WORMHOLE_B0:
+                brisc_program_default[0] = WORMHOLE_BRISC_BASE_INSTRUCTION;
+                break;
+            case tt::ARCH::BLACKHOLE:
+                brisc_program_default[0] = BLACKHOLE_BRISC_BASE_INSTRUCTION;
+                break;
+            default:
+                return;
+        }
+
         std::lock_guard<std::mutex> lock(mtx);
 
         for (auto& chip_id : cluster->get_target_device_ids()) {

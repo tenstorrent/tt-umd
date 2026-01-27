19995dd0f77fb732dfa1044f286274cb06a8c38b
Pavle Janevski <pjanevski@tenstorrent.com>
2026-01-27 12:44:56 +0000

    Add back python files

---
+++ nanobind/tests/test_py_cluster.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+import unittest
+import tt_umd
+
+class TestCluster(unittest.TestCase):
+    def test_cluster_functionality(self):
+        cluster = tt_umd.Cluster()
+        target_device_ids = cluster.get_target_device_ids()
+        print("Cluster device IDs:", target_device_ids)
+        clocks = cluster.get_clocks()
+        print("Cluster clocks:", clocks)

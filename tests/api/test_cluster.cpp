f1ec9e35d5967802148407892bb87e9288bbedb2
Pavle Janevski <pjanevski@tenstorrent.com>
2026-02-03 13:45:08 +0000

    Add test

--- tests/api/test_cluster.cpp
+++ tests/api/test_cluster.cpp
@@ -91,7 +91,7 @@ bool is_ipmitool_ready() {
 // This test should be one line only.
 TEST(ApiClusterTest, OpenAllSiliconChips) { std::unique_ptr<Cluster> umd_cluster = std::make_unique<Cluster>(); }
 
-TEST(ApiClusterTest, OpenChipsByPciId) {
+TEST(ApiClusterTest, OpenChipsById) {
     std::vector<int> pci_device_ids = PCIDevice::enumerate_devices();
 
     // T3K and 4U have 4 PCIE visible devices each. After 4 devices, the next number is 32
@@ -105,35 +105,34 @@ TEST(ApiClusterTest, OpenChipsByPciId) {
     int total_combinations = 1 << pci_device_ids.size();
 
     for (uint32_t combination = 0; combination < total_combinations; combination++) {
-        std::unordered_set<int> target_pci_device_ids;
+        std::unordered_set<int> target_device_ids;
         for (int i = 0; i < pci_device_ids.size(); i++) {
             if (combination & (1 << i)) {
-                target_pci_device_ids.insert(pci_device_ids[i]);
+                target_device_ids.insert(i);
             }
         }
 
-        std::cout << "Creating Cluster with target PCI device IDs: ";
-        for (const auto& id : target_pci_device_ids) {
+        std::cout << "Creating Cluster with target device IDs: ";
+        for (const auto& id : target_device_ids) {
             std::cout << id << " ";
         }
         std::cout << std::endl;
 
-        std::string value = test_utils::convert_to_comma_separated_string(target_pci_device_ids);
+        std::string value = test_utils::convert_to_comma_separated_string(target_device_ids);
 
         if (setenv(utils::TT_VISIBLE_DEVICES_ENV.data(), value.c_str(), 1) != 0) {
             ASSERT_TRUE(false) << "Failed to unset environment variable.";
         }
 
         // Make sure that Cluster construction is without exceptions.
-        // TODO: add cluster descriptors for expected topologies, compare cluster desc against expected desc.
         std::unique_ptr<Cluster> cluster = std::make_unique<Cluster>();
 
-        if (!target_pci_device_ids.empty()) {
-            // If target_pci_device_ids is empty, then full cluster will be created, so skip the check.
+        if (!target_device_ids.empty()) {
+            // If target_device_ids is empty, then full cluster will be created, so skip the check.
             // Check that the cluster has the expected number of chips.
             auto actual_pci_device_ids = cluster->get_target_mmio_device_ids();
-            EXPECT_EQ(actual_pci_device_ids.size(), target_pci_device_ids.size());
-            // Always expect logical id 0 to exist, that's the way filtering by pci ids work.
+            EXPECT_EQ(actual_pci_device_ids.size(), target_device_ids.size());
+            // Always expect logical id 0 to exist, that's the way filtering by ids work.
             EXPECT_TRUE(actual_pci_device_ids.find(0) != actual_pci_device_ids.end());
         }
 
@@ -237,6 +236,38 @@ TEST(ApiClusterTest, OpenChipsByBDFWormhole6U) {
     }
 }
 
+TEST(ApiClusterTest, OpenChipsByBDFWormhole6USameChip) {
+    // Get all available PCI devices and their BDF addresses.
+    std::unique_ptr<Cluster> cluster = std::make_unique<Cluster>();
+
+    if (cluster->get_target_device_ids().empty()) {
+        GTEST_SKIP() << "No PCI devices found for testing TT_VISIBLE_DEVICES";
+    }
+
+    if (cluster->get_tt_device(0)->get_board_type() != BoardType::UBB_WORMHOLE) {
+        GTEST_SKIP() << "This test is intended to be run on Wormhole 6U systems only.";
+    }
+
+    // This BDF and logical ID 0 should represent the same device on galaxy 6U system.
+    std::string filter_value = "0000:01:00.0,0";
+
+    if (setenv(utils::TT_VISIBLE_DEVICES_ENV.data(), filter_value.c_str(), 1) != 0) {
+        ASSERT_TRUE(false) << "Failed to set TT_VISIBLE_DEVICES environment variable.";
+    }
+
+    // Make sure that Cluster construction is without exceptions.
+    std::unique_ptr<Cluster> cluster_tt_visible_devices = std::make_unique<Cluster>();
+
+    // Check that the cluster has the expected number of chips, which is 1 because of the BDF being the lowest out of
+    // all chips on galaxy, which is at the same time represented by logical ID 0.
+    auto actual_pci_device_ids = cluster_tt_visible_devices->get_target_mmio_device_ids();
+    EXPECT_EQ(actual_pci_device_ids.size(), 1);
+
+    if (unsetenv(utils::TT_VISIBLE_DEVICES_ENV.data()) != 0) {
+        ASSERT_TRUE(false) << "Failed to unset TT_VISIBLE_DEVICES environment variable.";
+    }
+}
+
 TEST(ApiClusterTest, OpenChipsByBDFWormhole6UPattern) {
     // Get all available PCI devices and their BDF addresses.
     std::unique_ptr<Cluster> cluster = std::make_unique<Cluster>();
@@ -265,6 +296,32 @@ TEST(ApiClusterTest, OpenChipsByBDFWormhole6UPattern) {
 
     if (unsetenv(utils::TT_VISIBLE_DEVICES_ENV.data()) != 0) {
         ASSERT_TRUE(false) << "Failed to unset TT_VISIBLE_DEVICES environment variable.";
+    }
+}
+
+TEST(ApiClusterTest, OpenChipsByIdException) {
+    std::vector<int> pci_device_ids = PCIDevice::enumerate_devices();
+
+    std::unordered_set<int> target_device_ids;
+    target_device_ids.insert(pci_device_ids.size());
+
+    std::cout << "Creating Cluster with target device IDs: ";
+    for (const auto& id : target_device_ids) {
+        std::cout << id << " ";
+    }
+    std::cout << std::endl;
+
+    std::string value = test_utils::convert_to_comma_separated_string(target_device_ids);
+
+    if (setenv(utils::TT_VISIBLE_DEVICES_ENV.data(), value.c_str(), 1) != 0) {
+        ASSERT_TRUE(false) << "Failed to unset environment variable.";
+    }
+
+    // Since target ID is not in the range of available devices, expect an exception.
+    EXPECT_THROW(std::make_unique<Cluster>(), std::runtime_error);
+
+    if (unsetenv(utils::TT_VISIBLE_DEVICES_ENV.data()) != 0) {
+        ASSERT_TRUE(false) << "Failed to unset environment variable.";
     }
 }
 

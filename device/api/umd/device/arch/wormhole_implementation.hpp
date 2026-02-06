6034c14d8e360cac8ed98b185f8f467ceec6a917
Bojan Rosko <brosko@tenstorrent.com>
2026-02-06 10:07:54 +0000

    minor comment

--- device/api/umd/device/arch/wormhole_implementation.hpp
+++ device/api/umd/device/arch/wormhole_implementation.hpp
@@ -338,10 +338,11 @@ inline constexpr uint32_t SPI_PAGE_ERASE_SIZE = 0x1000;
 inline constexpr uint32_t SPI_ROM_SIZE = 1 << 24;
 inline constexpr uint32_t ARC_SPI_CHUNK_SIZE = SPI_PAGE_ERASE_SIZE;
 
-// Not connected: LINK_INACTIVE_TIMEOUT_SIGDET: 11
-// Not connected:LINK_INACTIVE_TIMEOUT_PG_RCV: 12
-// Unused: LINK_INACTIVE_PORT_NOT_POPULATED: 13
-// Port disabled: LINK_INACTIVE_PORT_MASKED_OFF: 14.
+// Not connected:     LINK_INACTIVE_TIMEOUT_SIGDET: 11
+// Not connected:     LINK_INACTIVE_TIMEOUT_PG_RCV: 12
+// Unused:        LINK_INACTIVE_PORT_NOT_POPULATED: 13
+// Port disabled:    LINK_INACTIVE_PORT_MASKED_OFF: 14
+// On GLX6U when I saw ETH train issues, the error status was 2, everything up to 4 is interpreted as config error.
 inline constexpr uint32_t ETH_LINK_UNUSED_ERROR_CODE_RANGE_START = 11;
 
 inline constexpr uint32_t ETH_TRAIN_STATUS_ADDR = 0x1104;

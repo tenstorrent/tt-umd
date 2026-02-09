02451c75cbefdfce69cab6b0decedb89cea5b819
Nenad Buncic <nbuncic@tenstorrent.com>
2026-02-09 15:09:09 +0000

    WIP: PCIe Protocol migration

--- device/api/umd/device/tt_device/protocol/pcie_protocol.hpp
+++ device/api/umd/device/tt_device/protocol/pcie_protocol.hpp
@@ -6,16 +6,41 @@
 #pragma once
 
 #include "device_protocol.hpp"
+#include "umd/device/pcie/pci_device.hpp"
+#include "umd/device/pcie/tlb_window.hpp"
 
 namespace tt::umd {
 
 class PcieProtocol : public DeviceProtocol {
 public:
-    PcieProtocol() = default;
+    PcieProtocol(std::shared_ptr<PCIDevice> pci_device, bool use_safe_api);
+
     virtual ~PcieProtocol() = default;
 
-    void write_to_device(const void* mem_ptr, tt_xy_pair core, uint64_t addr, uint32_t size) override;
-    void read_from_device(void* mem_ptr, tt_xy_pair core, uint64_t addr, uint32_t size) override;
+    void write_to_device(const void *mem_ptr, tt_xy_pair core, uint64_t addr, uint32_t size) override;
+
+    void read_from_device(void *mem_ptr, tt_xy_pair core, uint64_t addr, uint32_t size) override;
+
+    std::shared_ptr<PCIDevice> get_pci_device();
+
+private:
+    template <bool safe>
+    void write_to_device_impl(const void *mem_ptr, tt_xy_pair core, uint64_t addr, uint32_t size);
+
+    template <bool safe>
+    void read_from_device_impl(void *mem_ptr, tt_xy_pair core, uint64_t addr, uint32_t size);
+
+    TlbWindow *get_cached_tlb_window();
+
+    TlbWindow *get_cached_pcie_dma_tlb_window(tlb_data config);
+
+    std::unique_ptr<TlbWindow> cached_tlb_window = nullptr;
+
+    std::unique_ptr<TlbWindow> cached_pcie_dma_tlb_window = nullptr;
+
+    std::shared_ptr<PCIDevice> pci_device_;
+
+    bool use_safe_api_ = false;
 };
 
 }  // namespace tt::umd
--- device/tt_device/protocol/pcie_protocol.cpp
+++ device/tt_device/protocol/pcie_protocol.cpp
@@ -8,8 +8,51 @@
 
 namespace tt::umd {
 
-void PcieProtocol::write_to_device(const void* mem_ptr, tt_xy_pair core, uint64_t addr, uint32_t size) {}
+void PcieProtocol::write_to_device(const void *mem_ptr, tt_xy_pair core, uint64_t addr, uint32_t size) {
+    if (use_safe_api_) {
+        write_to_device_impl<true>(mem_ptr, core, addr, size);
+        return;
+    }
+    write_to_device_impl<false>(mem_ptr, core, addr, size);
+}
 
-void PcieProtocol::read_from_device(void* mem_ptr, tt_xy_pair core, uint64_t addr, uint32_t size) {}
+void PcieProtocol::read_from_device(void *mem_ptr, tt_xy_pair core, uint64_t addr, uint32_t size) {
+    if (use_safe_api_) {
+        read_from_device_impl<true>(mem_ptr, core, addr, size);
+        return;
+    }
+    read_from_device_impl<false>(mem_ptr, core, addr, size);
+}
+
+template <bool safe>
+void PcieProtocol::read_from_device_impl(void *mem_ptr, tt_xy_pair core, uint64_t addr, uint32_t size) {
+    if constexpr (safe) {
+        get_cached_tlb_window()->safe_read_block_reconfigure(mem_ptr, core, addr, size);
+    } else {
+        get_cached_tlb_window()->read_block_reconfigure(mem_ptr, core, addr, size);
+    }
+}
+
+template <bool safe>
+void PcieProtocol::write_to_device_impl(const void *mem_ptr, tt_xy_pair core, uint64_t addr, uint32_t size) {
+    if constexpr (safe) {
+        get_cached_tlb_window()->safe_write_block_reconfigure(mem_ptr, core, addr, size);
+    } else {
+        get_cached_tlb_window()->write_block_reconfigure(mem_ptr, core, addr, size);
+    }
+}
+
+std::shared_ptr<PCIDevice> PcieProtocol::get_pci_device() { return pci_device_; }
+
+TlbWindow *PcieProtocol::get_cached_pcie_dma_tlb_window(tlb_data config) {
+    if (cached_pcie_dma_tlb_window == nullptr) {
+        cached_pcie_dma_tlb_window =
+            std::make_unique<TlbWindow>(get_pci_device()->allocate_tlb(16 * 1024 * 1024, TlbMapping::WC), config);
+        return cached_pcie_dma_tlb_window.get();
+    }
+
+    cached_pcie_dma_tlb_window->configure(config);
+    return cached_pcie_dma_tlb_window.get();
+}
 
 }  // namespace tt::umd

4b9ecbd1608dbb3100ac592b3e674dfa8246afb2
Pavle Janevski <pjanevski@tenstorrent.com>
2026-02-19 09:18:07 +0000

    Remove unused includes

--- tests/api/test_risc_program.cpp
+++ tests/api/test_risc_program.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: © 2023 Tenstorrent Inc.
+// SPDX-FileCopyrightText: © 2026 Tenstorrent Inc.
 //
 // SPDX-License-Identifier: Apache-2.0
 
@@ -7,13 +7,10 @@
 #include <fmt/format.h>
 #include <fmt/xchar.h>
 #include <gtest/gtest.h>
-#include <sys/types.h>
-#include <unistd.h>  // For access()
 
 #include <algorithm>
 #include <array>
 #include <cstdint>
-#include <cstdlib>  // for std::getenv
 #include <cstring>
 #include <filesystem>
 #include <iostream>
@@ -27,10 +24,7 @@
 #include <unordered_set>
 #include <vector>
 
-#include "test_utils/assembly_programs_for_tests.hpp"
 #include "test_utils/setup_risc_cores.hpp"
-#include "tests/test_utils/device_test_utils.hpp"
-#include "tests/test_utils/fetch_local_files.hpp"
 #include "tests/test_utils/test_api_common.hpp"
 #include "umd/device/arch/blackhole_implementation.hpp"
 #include "umd/device/arch/grendel_implementation.hpp"
@@ -38,12 +32,10 @@
 #include "umd/device/chip/local_chip.hpp"
 #include "umd/device/chip/mock_chip.hpp"
 #include "umd/device/cluster.hpp"
-#include "umd/device/cluster_descriptor.hpp"
 #include "umd/device/firmware/erisc_firmware.hpp"
 #include "umd/device/firmware/firmware_utils.hpp"
 #include "umd/device/types/arch.hpp"
 #include "umd/device/types/cluster_descriptor_types.hpp"
-#include "umd/device/types/core_coordinates.hpp"
 #include "umd/device/types/risc_type.hpp"
 #include "umd/device/types/tensix_soft_reset_options.hpp"
 #include "umd/device/warm_reset.hpp"

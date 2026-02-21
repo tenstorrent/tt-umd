bb62ebe62d63a59f69d8d0f119c91207c50bb2cd
Aleksa Markovic <aleksamarkovic@tenstorrent.com>
2026-02-21 15:48:02 +0000

    Select architecture in TopologyDiscovery

--- device/topology/topology_discovery.cpp
+++ device/topology/topology_discovery.cpp
@@ -25,41 +25,38 @@
 #include "umd/device/firmware/firmware_info_provider.hpp"
 #include "umd/device/topology/topology_discovery.hpp"
 #include "umd/device/tt_device/tt_device.hpp"
+#include "umd/device/types/arch.hpp"
+#include "umd/device/types/communication_protocol.hpp"
 #include "umd/device/utils/semver.hpp"
 #include "umd/device/utils/timeouts.hpp"
 
 namespace tt::umd {
 
-std::unique_ptr<TopologyDiscovery> TopologyDiscovery::create_topology_discovery(
-    const TopologyDiscoveryOptions& options) {
-    tt::ARCH current_arch = ARCH::Invalid;
-
-    switch (options.io_device_type) {
+tt::ARCH TopologyDiscovery::determine_architecture(IODeviceType io_device_type) {
+    switch (io_device_type) {
         case IODeviceType::PCIe: {
             auto pci_devices_info = PCIDevice::enumerate_devices_info();
             if (pci_devices_info.empty()) {
-                return nullptr;
+                return tt::ARCH::Invalid;
             }
-            current_arch = pci_devices_info.begin()->second.get_arch();
-            break;
+            return pci_devices_info.begin()->second.get_arch();
         }
         case IODeviceType::JTAG: {
-            if (current_arch == tt::ARCH::BLACKHOLE) {
-                TT_THROW("Blackhole architecture is not yet supported over JTAG interface.");
-            }
-
             auto jtag_device = JtagDevice::create();
             if (!jtag_device->get_device_cnt()) {
-                return nullptr;
+                return tt::ARCH::Invalid;
             }
-            current_arch = jtag_device->get_jtag_arch(0);
-            break;
+            return jtag_device->get_jtag_arch(0);
         }
-        default:
-            TT_THROW("Unsupported device type for topology discovery");
+        case tt::umd::IODeviceType::UNDEFINED:
+            return tt::ARCH::Invalid;
     }
+}
 
-    switch (current_arch) {
+std::unique_ptr<TopologyDiscovery> TopologyDiscovery::create_topology_discovery(
+    const TopologyDiscoveryOptions& options) {
+    tt::ARCH architecture = options.architecture.value_or(determine_architecture(options.io_device_type));
+    switch (architecture) {
         case tt::ARCH::WORMHOLE_B0:
             return std::make_unique<TopologyDiscoveryWormhole>(options);
         case tt::ARCH::BLACKHOLE:
@@ -109,6 +106,10 @@ void TopologyDiscovery::get_connected_devices() {
 
     for (auto& device_id : local_device_ids) {
         std::unique_ptr<TTDevice> tt_device = TTDevice::create(device_id, options.io_device_type);
+        if (options.architecture.has_value() && tt_device->get_arch() != options.architecture) {
+            continue;
+        }
+
         // When coming out of reset, devices can take on the order of minutes to become ready.
         tt_device->init_tt_device(timeout::ARC_LONG_POST_RESET_TIMEOUT);
 

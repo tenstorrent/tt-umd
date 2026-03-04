6541f16ddb3de28eb3d66b80a439cb767c34d379
Pavle Janevski <pjanevski@tenstorrent.com>
2026-03-04 10:03:44 +0000

    Support warm reset API for UMD IDs and BDFs

--- tests/api/test_warm_reset.cpp
+++ tests/api/test_warm_reset.cpp
@@ -484,6 +484,155 @@ TEST(WarmResetTest, ClusterWarmReset) {
     }
 }
 
+TEST(WarmResetTest, ClusterWarmResetUMDIDs) {
+    if constexpr (is_arm_platform()) {
+        GTEST_SKIP() << "Warm reset is disabled on ARM64 due to instability.";
+    }
+    std::unique_ptr<Cluster> cluster = std::make_unique<Cluster>();
+
+    if (cluster->get_target_device_ids().empty()) {
+        GTEST_SKIP() << "No chips present on the system. Skipping test.";
+    }
+
+    if (is_galaxy_configuration(cluster.get())) {
+        GTEST_SKIP() << "Skipping test calling warm_reset() on Galaxy configurations.";
+    }
+
+    auto arch = cluster->get_tt_device(0)->get_arch();
+    if (arch == tt::ARCH::WORMHOLE_B0) {
+        GTEST_SKIP()
+            << "This test intentionally hangs the NOC. On Wormhole, this can cause a severe failure where even a warm "
+               "reset does not recover the device, requiring a watchdog-triggered reset for recovery.";
+    }
+
+    std::vector<uint8_t> data{1, 2, 3, 4, 5, 6, 7, 8};
+    std::vector<uint8_t> zero_data(data.size(), 0);
+    std::vector<uint8_t> readback_data(data.size(), 0);
+
+    // send data to core 15, 15 which will hang the NOC
+    auto hanged_chip_id = *cluster->get_target_device_ids().begin();
+    auto hanged_tt_device = cluster->get_chip(hanged_chip_id)->get_tt_device();
+    hanged_tt_device->write_to_device(data.data(), {15, 15}, 0, data.size());
+
+    // TODO: Remove this check when it is figured out why there is no hang detected on Blackhole.
+    if (arch == tt::ARCH::WORMHOLE_B0) {
+        EXPECT_THROW(hanged_tt_device->detect_hang_read(), std::runtime_error);
+    }
+
+    std::vector<int> umd_ids_warm_reset;
+    for (const auto& chip_id : cluster->get_target_mmio_device_ids()) {
+        umd_ids_warm_reset.push_back(chip_id);
+    }
+    WarmReset::warm_reset_umd_id(umd_ids_warm_reset);
+
+    cluster.reset();
+
+    cluster = std::make_unique<Cluster>();
+
+    EXPECT_FALSE(cluster->get_target_device_ids().empty()) << "No chips present after reset.";
+
+    // TODO: Comment this out after finding out how to detect hang reads on
+    // EXPECT_NO_THROW(cluster->get_chip(0)->get_tt_device()->detect_hang_read());.
+
+    auto chip_ids = cluster->get_target_device_ids();
+    for (auto& chip_id : chip_ids) {
+        auto tensix_cores = cluster->get_soc_descriptor(chip_id).get_cores(CoreType::TENSIX);
+
+        for (const CoreCoord& tensix_core : tensix_cores) {
+            RiscType select_all_tensix_riscv_cores{RiscType::ALL_TENSIX};
+
+            // Set all riscs to reset state.
+            cluster->assert_risc_reset(chip_id, tensix_core, select_all_tensix_riscv_cores);
+
+            cluster->l1_membar(chip_id, {tensix_core});
+
+            // Zero out first 8 bytes on L1.
+            cluster->write_to_device(zero_data.data(), zero_data.size(), chip_id, tensix_core, 0);
+
+            cluster->write_to_device(data.data(), data.size(), chip_id, tensix_core, 0);
+
+            cluster->read_from_device(readback_data.data(), chip_id, tensix_core, 0, readback_data.size());
+
+            ASSERT_EQ(data, readback_data);
+        }
+    }
+}
+
+TEST(WarmResetTest, ClusterWarmResetPCIBDFs) {
+    if constexpr (is_arm_platform()) {
+        GTEST_SKIP() << "Warm reset is disabled on ARM64 due to instability.";
+    }
+    std::unique_ptr<Cluster> cluster = std::make_unique<Cluster>();
+
+    if (cluster->get_target_device_ids().empty()) {
+        GTEST_SKIP() << "No chips present on the system. Skipping test.";
+    }
+
+    if (is_galaxy_configuration(cluster.get())) {
+        GTEST_SKIP() << "Skipping test calling warm_reset() on Galaxy configurations.";
+    }
+
+    auto arch = cluster->get_tt_device(0)->get_arch();
+    if (arch == tt::ARCH::WORMHOLE_B0) {
+        GTEST_SKIP()
+            << "This test intentionally hangs the NOC. On Wormhole, this can cause a severe failure where even a warm "
+               "reset does not recover the device, requiring a watchdog-triggered reset for recovery.";
+    }
+
+    std::vector<uint8_t> data{1, 2, 3, 4, 5, 6, 7, 8};
+    std::vector<uint8_t> zero_data(data.size(), 0);
+    std::vector<uint8_t> readback_data(data.size(), 0);
+
+    // send data to core 15, 15 which will hang the NOC
+    auto hanged_chip_id = *cluster->get_target_device_ids().begin();
+    auto hanged_tt_device = cluster->get_chip(hanged_chip_id)->get_tt_device();
+    hanged_tt_device->write_to_device(data.data(), {15, 15}, 0, data.size());
+
+    // TODO: Remove this check when it is figured out why there is no hang detected on Blackhole.
+    if (arch == tt::ARCH::WORMHOLE_B0) {
+        EXPECT_THROW(hanged_tt_device->detect_hang_read(), std::runtime_error);
+    }
+
+    auto pci_device_info = PCIDevice::enumerate_devices_info();
+    std::vector<std::string> pci_bdfs(pci_device_info.size());
+    for (const auto& [id, info] : pci_device_info) {
+        pci_bdfs.push_back(info.pci_bdf);
+    }
+    WarmReset::warm_reset_pci_bdfs(pci_bdfs);
+
+    cluster.reset();
+
+    cluster = std::make_unique<Cluster>();
+
+    EXPECT_FALSE(cluster->get_target_device_ids().empty()) << "No chips present after reset.";
+
+    // TODO: Comment this out after finding out how to detect hang reads on
+    // EXPECT_NO_THROW(cluster->get_chip(0)->get_tt_device()->detect_hang_read());.
+
+    auto chip_ids = cluster->get_target_device_ids();
+    for (auto& chip_id : chip_ids) {
+        auto tensix_cores = cluster->get_soc_descriptor(chip_id).get_cores(CoreType::TENSIX);
+
+        for (const CoreCoord& tensix_core : tensix_cores) {
+            RiscType select_all_tensix_riscv_cores{RiscType::ALL_TENSIX};
+
+            // Set all riscs to reset state.
+            cluster->assert_risc_reset(chip_id, tensix_core, select_all_tensix_riscv_cores);
+
+            cluster->l1_membar(chip_id, {tensix_core});
+
+            // Zero out first 8 bytes on L1.
+            cluster->write_to_device(zero_data.data(), zero_data.size(), chip_id, tensix_core, 0);
+
+            cluster->write_to_device(data.data(), data.size(), chip_id, tensix_core, 0);
+
+            cluster->read_from_device(readback_data.data(), chip_id, tensix_core, 0, readback_data.size());
+
+            ASSERT_EQ(data, readback_data);
+        }
+    }
+}
+
 class WarmResetNotificationTest : public ::testing::Test {
 public:
     static int run_child_monitor_logic(

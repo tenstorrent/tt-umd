f9f0becbaf9b7786e60f946225b5d42b6dfff61c
pjanevski <pjanevski@tenstorrent.com>
2026-03-11 10:27:59 +0000

    fix static cast

--- device/chip_helpers/simulation_sysmem_manager.cpp
+++ device/chip_helpers/simulation_sysmem_manager.cpp
@@ -42,7 +42,8 @@ bool SimulationSysmemManager::init_sysmem(uint32_t num_host_mem_channels) {
         total_size -= 256 * (1ULL << 20);
     }
 
-    system_memory_ = (uint8_t *)mmap(nullptr, total_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    system_memory_ =
+        static_cast<uint8_t *>(mmap(nullptr, total_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
     TT_ASSERT(system_memory_ != MAP_FAILED, "system_memory mmap() failed");
     system_memory_size_ = total_size;
 

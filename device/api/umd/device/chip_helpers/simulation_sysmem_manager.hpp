7d28be38a0a04d3aa4b20cf254f62322fb016c42
pjanevski <pjanevski@tenstorrent.com>
2026-03-12 14:54:30 +0000

    Cleanup

--- device/api/umd/device/chip_helpers/simulation_sysmem_manager.hpp
+++ device/api/umd/device/chip_helpers/simulation_sysmem_manager.hpp
@@ -5,12 +5,13 @@
 #pragma once
 
 #include "umd/device/chip_helpers/sysmem_manager.hpp"
+#include "umd/device/types/arch.hpp"
 
 namespace tt::umd {
 
 class SimulationSysmemManager : public SysmemManager {
 public:
-    SimulationSysmemManager(uint32_t num_host_mem_channels);
+    SimulationSysmemManager(uint32_t num_host_mem_channels, tt::ARCH arch = tt::ARCH::WORMHOLE_B0);
     ~SimulationSysmemManager() override;
 
     bool pin_or_map_sysmem_to_device() override;
--- device/chip_helpers/simulation_sysmem_manager.cpp
+++ device/chip_helpers/simulation_sysmem_manager.cpp
@@ -21,8 +21,8 @@
 
 namespace tt::umd {
 
-SimulationSysmemManager::SimulationSysmemManager(uint32_t num_host_mem_channels) {
-    pcie_base_ = 0x800000000;
+SimulationSysmemManager::SimulationSysmemManager(uint32_t num_host_mem_channels, tt::ARCH arch) {
+    pcie_base_ = arch == tt::ARCH::BLACKHOLE ? 4ULL << 58 : 0x800000000;
     SimulationSysmemManager::init_sysmem(num_host_mem_channels);
 }
 

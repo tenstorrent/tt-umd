bd53cd73aa952eb6b9b3273ceaa96033e7327f9d
Nenad Buncic <nbuncic@tenstorrent.com>
2026-03-12 07:33:38 +0000

    Address PR comments 2

--- device/tt_device/protocol/jtag_protocol.cpp
+++ device/tt_device/protocol/jtag_protocol.cpp
@@ -12,7 +12,7 @@
 
 namespace tt::umd {
 
-JtagProtocol::JtagProtocol(std::shared_ptr<JtagDevice> jtag_device, [[maybe_unused]] uint8_t jlink_id) :
+JtagProtocol::JtagProtocol(std::shared_ptr<JtagDevice> jtag_device, uint8_t jlink_id) :
     jtag_device_(std::move(jtag_device)) {}
 
 void JtagProtocol::write_to_device(const void*, tt_xy_pair, uint64_t, uint32_t) {

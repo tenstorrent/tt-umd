0f37c2ba3655f6c9f81f78715eba3eb9d6a12805
Aleksa Marković <aleksamarkovic@tenstorrent.com>
2026-03-13 14:05:47 +0100

    Fix ETH heartbeat startup wait (#2285)
    ### Issue
    #2162 
    
    ### Description
    This PR fixes a long waits when we can determine an ETH core is
    completely stuck and there is no need to wait 10s for startup.
    
    ### List of the changes
    - Introduced separate timeout for ETH startup and ETH heartbeat change.
    - Separate wait loops for startup and heartbeat in
    `is_heartbeat_running()`
    
    ### Testing
    CI
    Manually tested on bh-glx6u-16.
    
    ### API Changes
    There are no API changes in this PR.

--- device/topology/topology_discovery.cpp
+++ device/topology/topology_discovery.cpp
@@ -536,37 +536,62 @@ SocDescriptor TopologyDiscovery::get_soc_descriptor(TTDevice* tt_device) {
 bool TopologyDiscovery::eth_heartbeat_running(TTDevice* tt_device, tt_xy_pair eth_core) {
     const auto start = std::chrono::steady_clock::now();
     uint32_t previous_reading = 0;
+    // First loop: Wait until heartbeat changes from 0 (post reset).
     while (true) {
         uint32_t current_reading = get_eth_heartbeat(tt_device, eth_core);
 
-        // ERISC FW might take a long time to start up after warm reset.
-        // The value being read is 0 until ERISC FW starts.
-        if (current_reading != 0 && previous_reading != 0) {
-            // Heartbeat must be in the format 0xABCDxxxx.
-            if ((current_reading >> 16) != 0xABCD) {
-                log_warning(
-                    LogUMD,
-                    "Read invalid heartbeat value: {} from ETH core: {}, FW possibly corrupted.",
-                    current_reading,
-                    eth_core.str());
-                return false;
-            }
-            if (previous_reading != current_reading) {
-                return true;
-            }
+        if (current_reading != 0) {
+            previous_reading = current_reading;
+            break;
         }
+
         if (utils::check_timeout(
                 start,
                 timeout::ETH_STARTUP_TIMEOUT,
-                "Timed out waiting for ETH heartbeat.",
+                fmt::format(
+                    "Timed out waiting for ETH heartbeat on core {} to start. Stuck at {:#x}",
+                    eth_core.str(),
+                    current_reading),
                 utils::TimeoutAction::Return)) {
             return false;
         }
-        previous_reading = current_reading;
 
         std::this_thread::sleep_for(std::chrono::microseconds(10));
     }
-    return false;
+
+    // Second loop: Wait for heartbeat to change.
+    const auto second_start = std::chrono::steady_clock::now();
+    while (true) {
+        uint32_t current_reading = get_eth_heartbeat(tt_device, eth_core);
+
+        // Heartbeat must be in the format 0xABCDxxxx.
+        if ((current_reading >> 16) != 0xABCD) {
+            log_warning(
+                LogUMD,
+                "Read invalid heartbeat value: {:#x} from ETH core: {}, FW possibly corrupted.",
+                current_reading,
+                eth_core.str());
+            return false;
+        }
+
+        if (previous_reading != current_reading) {
+            return true;
+        }
+
+        if (utils::check_timeout(
+                second_start,
+                timeout::ETH_HEARTBEAT_TIMEOUT,
+                fmt::format(
+                    "Timed out waiting for ETH heartbeat on core {} to advance. Stuck at {:#x} -> {:#x}",
+                    eth_core.str(),
+                    previous_reading,
+                    current_reading),
+                utils::TimeoutAction::Return)) {
+            return false;
+        }
+
+        std::this_thread::sleep_for(std::chrono::microseconds(10));
+    }
 }
 
 }  // namespace tt::umd

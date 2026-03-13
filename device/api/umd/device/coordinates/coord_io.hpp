cc8492de5341a9ff77c3f6c1b35d25de14a63b8d
Aleksa Markovic <aleksamarkovic@tenstorrent.com>
2026-03-13 13:46:38 +0000

    fat class

--- device/api/umd/device/coordinates/coord_io.hpp
+++ device/api/umd/device/coordinates/coord_io.hpp
@@ -17,6 +17,14 @@ public:
     void read_from_device(void* mem_ptr, CoreCoord core, uint64_t addr, uint32_t size);
     void write_to_device(const void* mem_ptr, CoreCoord core, uint64_t addr, uint32_t size);
     void noc_multicast_write(void* dst, size_t size, CoreCoord core_start, CoreCoord core_end, uint64_t addr);
+    std::chrono::milliseconds wait_eth_core_training(
+        CoreCoord eth_core, const std::chrono::milliseconds timeout_ms = timeout::ETH_TRAINING_TIMEOUT);
+    uint32_t get_risc_reset_state(CoreCoord core);
+    void set_risc_reset_state(CoreCoord core, const uint32_t risc_flags);
+    void dma_write_to_device(const void* src, size_t size, CoreCoord core, uint64_t addr);
+    void dma_read_from_device(void* dst, size_t size, CoreCoord core, uint64_t addr);
+    void dma_multicast_write(void* src, size_t size, CoreCoord core_start, CoreCoord core_end, uint64_t addr);
+    EthTrainingStatus read_eth_core_training_status(CoreCoord eth_core);
 
 private:
     TTDevice* tt_device_ = nullptr;

efa2058a98b0c6e4ec71383bde55cfddba1f17b9
pjanevski <pjanevski@tenstorrent.com>
2026-03-30 14:10:25 +0000

    Address comments

--- device/chip/local_chip.cpp
+++ device/chip/local_chip.cpp
@@ -44,22 +44,22 @@ std::unique_ptr<LocalChip> LocalChip::create(
         soc_descriptor = SocDescriptor(sdesc_path, tt_device->get_chip_info());
     }
 
-    return LocalChip::create(std::move(tt_device), std::move(soc_descriptor), num_host_mem_channels);
+    return LocalChip::create(std::move(tt_device), soc_descriptor, num_host_mem_channels);
 }
 
 std::unique_ptr<LocalChip> LocalChip::create(
-    int physical_device_id, SocDescriptor soc_descriptor, int num_host_mem_channels, IODeviceType device_type) {
+    int physical_device_id, const SocDescriptor& soc_descriptor, int num_host_mem_channels, IODeviceType device_type) {
     // Create TTDevice and make sure the arc is ready so we can read its telemetry.
     // physical_device_id is not actually physical for JTAG devices here.
     // It represents the index within a vector of jlink devices discovered by JtagDevice.
     auto tt_device = TTDevice::create(physical_device_id, device_type);
     tt_device->init_tt_device();
 
-    return LocalChip::create(std::move(tt_device), std::move(soc_descriptor), num_host_mem_channels);
+    return LocalChip::create(std::move(tt_device), soc_descriptor, num_host_mem_channels);
 }
 
 std::unique_ptr<LocalChip> LocalChip::create(
-    std::unique_ptr<TTDevice> tt_device, SocDescriptor soc_descriptor, int num_host_mem_channels) {
+    std::unique_ptr<TTDevice> tt_device, const SocDescriptor& soc_descriptor, int num_host_mem_channels) {
     std::unique_ptr<TLBManager> tlb_manager = nullptr;
     std::unique_ptr<SysmemManager> sysmem_manager = nullptr;
     std::unique_ptr<RemoteCommunication> remote_communication = nullptr;
@@ -76,7 +76,7 @@ std::unique_ptr<LocalChip> LocalChip::create(
     remote_communication = RemoteCommunication::create_remote_communication(tt_device.get(), {0, 0, 0, 0}, sysmem_ptr);
 
     return std::unique_ptr<LocalChip>(new LocalChip(
-        std::move(soc_descriptor),
+        soc_descriptor,
         std::move(tt_device),
         std::move(tlb_manager),
         std::move(sysmem_manager),

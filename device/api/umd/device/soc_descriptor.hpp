e62b52adf7c78366b7158ca22c215bad81997cf4
Aleksa Markovic <aleksamarkovic@tenstorrent.com>
2026-05-01 10:34:51 +0000

    revert default ctor delete

--- device/api/umd/device/soc_descriptor.hpp
+++ device/api/umd/device/soc_descriptor.hpp
@@ -36,6 +36,7 @@ class SocArchDescriptor;
 */
 class SocDescriptor {
 public:
+    SocDescriptor() = default;  // TODO(#331): Delete when metal_SocDescriptor is deleted.
     SocDescriptor(std::shared_ptr<const SocArchDescriptor> arch_desc, const ChipInfo chip_info = {});
 
     // Helpers for extracting info from soc descriptor file.

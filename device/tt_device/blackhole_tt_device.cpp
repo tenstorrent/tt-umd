7a30f209ada4abfcbcef986e5493a33cd384e4fd
Bojan Roško <156314064+broskoTT@users.noreply.github.com>
2026-05-09 12:59:52 +0000

    format fix

--- device/tt_device/blackhole_tt_device.cpp
+++ device/tt_device/blackhole_tt_device.cpp
@@ -321,7 +321,9 @@ void BlackholeTTDevice::noc_multicast_write(void *src, size_t size, uint64_t add
     //                           NOC1: start=(1,2), end=(2,3).
     xy_pair start_coord;
     xy_pair end_coord;
-    UMD_ASSERT(get_chip_info().noc_translation_enabled, "Multicast not implemented for BH devices without NOC translation enabled.");
+    UMD_ASSERT(
+        get_chip_info().noc_translation_enabled,
+        "Multicast not implemented for BH devices without NOC translation enabled.");
     if (is_selected_noc1()) {
         start_coord = xy_pair{1, 2};
         end_coord = xy_pair{2, 3};

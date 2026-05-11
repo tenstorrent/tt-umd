f24e5d31f58c0a5ae4bb07329dfa73742699a749
Vuk Jovanovic <vjovanovic@tenstorrent.com>
2026-05-11 14:14:54 +0000

    Add use_safe_api option to TopologyDiscoveryOptions and expose it in Python API

--- nanobind/py_api_topology_discovery.cpp
+++ nanobind/py_api_topology_discovery.cpp
@@ -140,7 +140,8 @@ void bind_topology_discovery(nb::module_& m) {
         .def_rw("wait_on_ethernet_link_training", &TopologyDiscoveryOptions::wait_on_ethernet_link_training)
         .def_rw("perform_6u_eth_retrain", &TopologyDiscoveryOptions::perform_6u_eth_retrain)
         // Low power mode is temporarily disabled. See https://github.com/tenstorrent/tt-umd/issues/2531.
-        .def_rw("low_power", &TopologyDiscoveryOptions::low_power);
+        .def_rw("low_power", &TopologyDiscoveryOptions::low_power)
+        .def_rw("use_safe_api", &TopologyDiscoveryOptions::use_safe_api);
 
     nb::class_<TopologyDiscovery>(m, "TopologyDiscovery")
         .def_static(

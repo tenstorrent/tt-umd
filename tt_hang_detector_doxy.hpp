9f980ed93d74f0ad6435340e78202f75eae86686
Nenad Buncic <nbuncic@tenstorrent.com>
2026-05-28 05:34:46 +0000

    DOXY: HangDetector intro

---
+++ tt_hang_detector_doxy.hpp
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <cstdint>
+
+namespace tt::umd {
+
+class HangDetector {
+public:
+    virtual ~HangDetector() = default;
+
+    std::optional<bool> is_bus_hung(uint32_t data_read = HANG_READ_VALUE);
+    std::optional<bool> is_noc_hung(NocId noc);
+
+protected:
+    virtual uint32_t read_hang_check_reg_via_bus() = 0;
+    virtual uint32_t read_hang_check_reg_via_noc(NocId noc) = 0;
+};
+
+}  // namespace tt::umd

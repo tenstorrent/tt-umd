f844305103a8d655f427b40df9e1d9c36c65bda8
Aleksa Markovic <aleksamarkovic@tenstorrent.com>
2026-06-17 12:53:22 +0000

    wip2

--- tools/repro.cpp
+++ tools/repro.cpp
@@ -23,10 +23,10 @@ using namespace tt::umd;
 
 int main(int argc, char* argv[]) {
     NocIdSwitcher noc1(NocId::NOC1);
-
+    auto [cd, devs] = TopologyDiscovery::discover();
     for (uint32_t i = 0; i < 1000; i++) {
-        auto [cd, devs] = TopologyDiscovery::discover();
         for (auto& [id, device] : devs) {
+            device->set_power_state(i % 2 == 0);
             for (uint32_t i = 0; i < 100; i++) {
                 volatile uint32_t reset_state = device->get_risc_reset_state(CoreCoord(1, 2));
                 device->is_pcie_hung(reset_state);

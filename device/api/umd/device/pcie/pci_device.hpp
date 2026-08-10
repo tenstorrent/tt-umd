da9d429f7f85deee8934f23c608ddb3052308b47
Aleksa Markovic <aleksamarkovic@tenstorrent.com>
2026-08-10 12:25:56 +0000

    review

--- device/api/umd/device/pcie/pci_device.hpp
+++ device/api/umd/device/pcie/pci_device.hpp
@@ -23,11 +23,8 @@
 #include "umd/device/pcie/tlb_handle.hpp"
 #include "umd/device/types/arch.hpp"
 #include "umd/device/types/tlb.hpp"
-#include "umd/device/types/xy_pair.hpp"
 #include "umd/device/utils/semver.hpp"
 
-// Opaque handle owned by tt-kmd-lib, a private dependency. Forward-declared here
-// so this public header does not pull in (or leak) the tt-kmd-lib include path.
 struct tt_device_t;
 
 namespace tt::umd {

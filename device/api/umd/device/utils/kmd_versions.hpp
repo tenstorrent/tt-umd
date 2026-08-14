24cc6bcb9f313d3b2d30260056e34f47eb81c407
Aleksa Markovic <aleksamarkovic@tenstorrent.com>
2026-08-14 09:49:54 +0000

    Remove KMD version markers older than 2.0.0

--- device/api/umd/device/utils/kmd_versions.hpp
+++ device/api/umd/device/utils/kmd_versions.hpp
@@ -14,12 +14,6 @@ namespace tt::umd {
 inline constexpr SemVer KMD_MINIMUM_VERSION = SemVer(2, 0, 0);
 
 /**
- * KMD version 1.29.0 introduced IOMMU support. UMD requires at least this version to run with IOMMU enabled.
- * With newer versions of KMD, UMD will still work when IOMMU is disabled on the system.
- */
-inline constexpr SemVer KMD_IOMMU = SemVer(1, 29, 0);
-
-/**
  * KMD version 2.0.0 introduced support for mapping buffers to NOC by using IOCTL. Before 2.0.0, UMD used to access
  * iATU configuration registers directly to perform such mappings. KMD exposed this functionality via IOCTL which brings
  * the ability to map buffers from multiple processes safely. While it's still possible to use direct register access
@@ -32,13 +26,6 @@ inline constexpr SemVer KMD_MAP_TO_NOC = SemVer(2, 0, 0);
  * IOCTL UMD can now reset different architectures without needing to have architecture specific reset IOCTLs.
  */
 inline constexpr SemVer KMD_ARCH_AGNOSTIC_RESET = SemVer{2, 4, 1};
-
-/**
- * KMD version 1.34.0 introduced support for configuring and using PCIe TLBs for buffer mappings. This feature enables
- * calls into KMD to reserve TLB by size, in order to enable multiple user processes to use the device safely at the
- * same time.
- */
-inline constexpr SemVer KMD_TLBS = SemVer(1, 34, 0);
 
 /**
  * KMD version 2.6.0 introduced the TENSTORRENT_IOCTL_SET_POWER_STATE IOCTL for explicit per-client power domain

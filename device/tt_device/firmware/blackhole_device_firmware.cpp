85e2ca351d793d98db5360b36aebf9aaef9d0d13
Bojan Rosko <brosko@tenstorrent.com>
2026-08-28 18:39:55 +0000

    precommit fix

--- device/tt_device/firmware/blackhole_device_firmware.cpp
+++ device/tt_device/firmware/blackhole_device_firmware.cpp
@@ -24,8 +24,8 @@
 #include "umd/device/types/blackhole_arc.hpp"
 #include "umd/device/types/blackhole_eth.hpp"
 #include "umd/device/types/telemetry.hpp"
-#include "umd/device/utils/lock_manager.hpp"
 #include "umd/device/utils/common.hpp"
+#include "umd/device/utils/lock_manager.hpp"
 #include "umd/device/utils/error.hpp"
 #include "utils.hpp"
 
